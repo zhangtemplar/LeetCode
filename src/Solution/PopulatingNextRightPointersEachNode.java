2af4aa8342e750dd8b2efbf91670a46c70a7dabe
zhangtemplar <zhangtemplar@gmail.com>
2013-10-07 22:28:52 -0700

    Update PopulatingNextRightPointersEachNode.java

--- src/Solution/PopulatingNextRightPointersEachNode.java
+++ src/Solution/PopulatingNextRightPointersEachNode.java
@@ -1,67 +1,66 @@
-package Solution;
-
-import java.util.HashMap;
-import java.util.LinkedList;
-
-public class PopulatingNextRightPointersEachNode {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void connect(TreeLinkNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we can a level order traversal
-        // use level-order traverse
-        LinkedList<TreeLinkNode> queue=new LinkedList<TreeLinkNode>();
-        HashMap<TreeLinkNode, Integer> level=new HashMap<TreeLinkNode, Integer>();
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we will use level order traverse
         if (root==null)
         {
             return;
         }
+        // depth
+        HashMap<TreeLinkNode, Integer> depth=new HashMap<TreeLinkNode, Integer>();
+        // node to be processed
+        LinkedList<TreeLinkNode> queue=new LinkedList<TreeLinkNode>();
+        // to store the node of current level
+        ArrayList<TreeLinkNode> level=new ArrayList<TreeLinkNode>();
+        int current_depth=1;
+        depth.put(root, 1);
         queue.add(root);
-        level.put(root, 0);
-        TreeLinkNode node, prev;
-        node=null;
-        prev=null;
-        int height=0;
+        TreeLinkNode node=null;
+        TreeLinkNode ptr=null;
         while(!queue.isEmpty())
         {
             node=queue.remove();
-            // add its children and their height accordingly
+            if (depth.get(node)!=current_depth)
+            {
+                // we finish one level, process it
+                for (int i=0; i<level.size()-1; i++)
+                {
+                    ptr=level.get(i);
+                    ptr.next=level.get(i+1);
+                }
+                ptr=level.get(level.size()-1);
+                ptr.next=null;
+                // clear the buffer
+                current_depth=depth.get(node);
+                level=new ArrayList<TreeLinkNode>();
+            }
+            level.add(node);
             if (node.left!=null)
             {
                 queue.add(node.left);
-                level.put(node.left, level.get(node)+1);
+                depth.put(node.left, depth.get(node)+1);
             }
             if (node.right!=null)
             {
                 queue.add(node.right);
-                level.put(node.right, level.get(node)+1);
+                depth.put(node.right, depth.get(node)+1);
             }
-            // check whether we have finished the level or not
-            if (level.get(node)!=height)
-            {
-                // we finish the previous level
-                prev=null;
-                height=level.get(node);
-            }
-            if (prev!=null)
-            {
-                prev.next=node;
-            }
-            prev=node;
         }
+        // for the last level
+        for (int i=0; i<level.size()-1; i++)
+        {
+            ptr=level.get(i);
+            ptr.next=level.get(i+1);
+        }
+        ptr=level.get(level.size()-1);
+        ptr.next=null;
     }
-}
-
-class TreeLinkNode {
-    int val;
-    TreeLinkNode left, right, next;
-    TreeLinkNode(int x) { val = x; }
 }
