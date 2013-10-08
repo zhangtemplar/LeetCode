c7ed2d0082ce8e9047b9e5d2046418d381a7c6e3
zhangtemplar <zhangtemplar@gmail.com>
2013-10-07 21:20:20 -0700

    Update BestTimeButAndSellStockII.java

--- src/Solution/BestTimeButAndSellStockII.java
+++ src/Solution/BestTimeButAndSellStockII.java
@@ -1,84 +1,45 @@
-package Solution;
-
-public class BestTimeButAndSellStockII {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public int maxProfit(int[] prices) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // now problems becomes find a set of pairs of buy and sell operation
-        // the basic idea is that, we buy the stock if it is going to increase
-        // when we find the price is going to drop, we sell it out
-        // at buy it back at the next position
+public class Solution {
+    public int maxProfit(int[] prices) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // the basic idea is, we buy the stock when the price is going to increase
+        // we sell it when the price is going to drop
         if (prices==null || prices.length<2)
         {
             return 0;
         }
-        else if(prices.length==2)
-        {
-            if (prices[1]-prices[0]>0)
-            {
-                return prices[1]-prices[0];
-            }
-            else
-            {
-                return 0;
-            }
-        }
+        // flag for whether we have the stock or not
+        boolean is_bought=false;
         int profit=0;
-        int buy_price=-1;
-        int sell_price=-1;
-        // this indicates that we already has the stock
-        boolean is_sold=true;
-        for (int i=1; i<prices.length-1; i++)
+        int price_buy=0;
+        int price_sell=0;
+        for (int i=0; i<prices.length-1; i++)
         {
-            // we find the price will increase
-            // thus we buy in the stock
-            if (prices[i]>prices[i-1] && is_sold) 
+            // buy
+            if (prices[i]<prices[i+1])
             {
-                // buy the stock
-                buy_price=prices[i-1];
-                is_sold=false;
+                if (is_bought==false)
+                {
+                    price_buy=prices[i];
+                    is_bought=true;
+                }
             }
-            // we find the price will decrease
-            // thus we sell off the stock (if it is possible)
-            if (prices[i]>prices[i+1] && !is_sold)
+            // sell
+            // else is used to avoid the sell and buy at the same lot
+            else if (prices[i]>prices[i+1])
             {
-                // sell the stock
-                sell_price=prices[i];
-                is_sold=true;
-                // record this profit
-                profit+=sell_price-buy_price;
-                // clear history
-                // we assume the price is nonegative
-                sell_price=-1;
-                buy_price=-1;
+                if (is_bought==true)
+                {
+                    price_sell=prices[i];
+                    is_bought=false;
+                    profit+=price_sell-price_buy;
+                }
             }
         }
-        // track whether the last transaction is finised or not
-        // i.e., we have already bought the stock but haven't sold it
-        if (buy_price<0)
+        // finally, we need to check whether we still hold the stock or not
+        if (is_bought==true && prices[prices.length-1]>price_buy)
         {
-            // we only check the buy_price for [0: n-3)
-            if (prices[prices.length-2]<prices[prices.length-1])
-            {
-                profit+=prices[prices.length-1]-prices[prices.length-2];
-            }
-        }
-        else if (sell_price<0)
-        {
-            // we need to decide whether this transaction is processed
-            if (buy_price<prices[prices.length-1])
-            {
-                profit+=prices[prices.length-1]-buy_price;
-            }
+            profit+=prices[prices.length-1]-price_buy;
+            is_bought=false;
         }
         return profit;
     }
