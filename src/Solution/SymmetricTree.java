a8ea6d90c94a6aace56eab9302ab6872fc7d3a7f
zhangtemplar <zhangtemplar@gmail.com>
2013-10-07 22:01:49 -0700

    Update SymmetricTree.java
    Submission Result: Wrong Answer
    
    Input:	{1}
    Output:	false
    Expected:	true
    The reason is that, in L41 and L92, we are comparing object (Long) instead of primitive type (long), we should use equals(), instead of == for object.

--- src/Solution/SymmetricTree.java
+++ src/Solution/SymmetricTree.java
@@ -1,97 +1,98 @@
-package Solution;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-
-public class SymmetricTree {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TreeNode root=new TreeNode(1);
-		root.left=new TreeNode(2);
-		root.right=new TreeNode(2);
-		root.left.right=new TreeNode(3);
-		root.right.left=new TreeNode(3);
-		System.out.println(root);
-		SymmetricTree instance=new SymmetricTree();
-		System.out.println(instance.isSymmetric(root));
-	}
-
-	public boolean isSymmetric(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we simplest way is of course using recursion
-        // however, we can also use stack to make it iteratively
-        // we will perform level order traverse
-        LinkedList<TreeNode> queue=new LinkedList<TreeNode>();
-        HashMap<TreeNode, Integer> level=new HashMap<TreeNode, Integer>();
-        ArrayList<Integer> value=new ArrayList<Integer>();
-        if (root==null)
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we can use a level order traverse
+        // to mark the null node, we will use Long.MAX_VALUE
+        if (root==null || (root.left==null && root.right==null))
         {
             return true;
         }
+        // the value of nodes at the previous level
+        ArrayList<Long> value=new ArrayList<Long>();
+        // the list of queues to be visited
+        LinkedList<TreeNode> queue=new LinkedList<TreeNode>();
+        // to store the height
+        HashMap<TreeNode, Integer> depth=new HashMap<TreeNode, Integer>();
+        // to store the flag for a null node
+        HashMap<TreeNode, Boolean> flag=new HashMap<TreeNode, Boolean>();
+        depth.put(root, 1);
         queue.add(root);
-        level.put(root, 0);
-        int height=0;
+        flag.put(root, true);
+        int depth_current=1;
+        TreeNode node=null;
         while(!queue.isEmpty())
         {
-            TreeNode node=queue.remove();
-            if (node.val!=Integer.MIN_VALUE)
+            node=queue.remove();
+            // check whether we have finished a level or not
+            if (depth_current!=depth.get(node))
             {
-                // add its children and their height accordingly
+                // we have finished a level
+                for (int i=0; i<value.size()-1-i; i++)
+                {
+                    if (!value.get(i).equals(value.get(value.size()-1-i)))
+                    {
+                        return false;
+                    }
+                }
+                // so far so good, we start for the next level
+                depth_current=depth.get(node);
+                value=new ArrayList<Long>();
+            }
+            // check its children if this node is a fake node
+            if (flag.get(node))
+            {
+                // add the value to the queue
+                value.add((long) node.val);
+                // add the children in
                 if (node.left!=null)
                 {
                     queue.add(node.left);
-                    level.put(node.left, level.get(node)+1);
+                    depth.put(node.left, depth.get(node)+1);
+                    flag.put(node.left, true);
                 }
                 else
                 {
-                    TreeNode fake=new TreeNode(Integer.MIN_VALUE);
-                    queue.add(fake);
-                    level.put(fake, level.get(node)+1);
+                    TreeNode fake_node=new TreeNode(0);
+                    queue.add(fake_node);
+                    depth.put(fake_node, depth.get(node)+1);
+                    flag.put(fake_node, false);
                 }
                 if (node.right!=null)
                 {
                     queue.add(node.right);
-                    level.put(node.right, level.get(node)+1);
+                    depth.put(node.right, depth.get(node)+1);
+                    flag.put(node.right, true);
                 }
                 else
                 {
-                    TreeNode fake=new TreeNode(Integer.MIN_VALUE);
-                    queue.add(fake);
-                    level.put(fake, level.get(node)+1);
+                    TreeNode fake_node=new TreeNode(0);
+                    queue.add(fake_node);
+                    depth.put(fake_node, depth.get(node)+1);
+                    flag.put(fake_node, false);
                 }
             }
-            // check whether we have finished the level or not
-            if (level.get(node)!=height)
+            else
             {
-                // we finish the previous level
-                // check wheher it is symmetric
-                if (height!=0 && value.size()%2!=0)
-                {
-                    return false;
-                }
-                else
-                {
-                    // check the value
-                    for (int i=0; i<value.size()-i; i++)
-                    {
-                    	// note we can't use value.get(i).!=value.get(value.size()-i-1) here
-                    	// because we are dealing objects, which are compared according to hashcode
-                        if (!value.get(i).equals(value.get(value.size()-i-1)))
-                        {
-                            return false;
-                        }
-                    }
-                }
-                height=level.get(node);
-                value.clear();
+                // for a fake node, we just add some value which can't exist
+                value.add(Long.MAX_VALUE);
             }
-            value.add(node.val);
+        }
+        // finish the leaf nodes
+        for (int i=0; i<value.size()-1-i; i++)
+        {
+            if (!value.get(i).equals(value.get(value.size()-1-i)))
+            {
+                return false;
+            }
         }
         return true;
     }
