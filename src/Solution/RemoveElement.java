42be1732d66dfafef0fb9e3bd4d8e1c4fac85695
zhangtemplar <zhangtemplar@gmail.com>
2013-10-07 22:55:50 -0700

    Update RemoveElement.java

--- src/Solution/RemoveElement.java
+++ src/Solution/RemoveElement.java
@@ -1,31 +1,21 @@
-package Solution;
-
-public class RemoveElement {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public int removeElement(int[] A, int elem) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // the first available lot and also the length of array
-        int count=0;
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we maintain two pointer, one for the first empty lot and the other for the current processed lot
         if (A==null || A.length<1)
         {
-            return count;
+            return 0;
         }
+        int available=0;
         for (int i=0; i<A.length; i++)
         {
+            // we found a different element save it to the first empty lot
             if (A[i]!=elem)
             {
-                A[count++]=A[i];
+                A[available]=A[i];
+                available++;
             }
         }
-        return count;
+        return available;
     }
 }
