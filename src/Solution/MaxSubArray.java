6ba9663969192081598e5a41aa39ce2e4a62d551
zhangtemplar <zhangtemplar@gmail.com>
2013-10-07 23:01:52 -0700

    Update MaxSubArray.java

--- src/Solution/MaxSubArray.java
+++ src/Solution/MaxSubArray.java
@@ -1,40 +1,25 @@
-package Solution;
-
-public class MaxSubArray {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public int maxSubArray(int[] A) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // it is the famous max sum problem
-        // we keep two variables
-        //  max_sum, which is the maximal of the sum of an continuous array
-        //  cur_sum, which is the cumultive sum from the end of last continuous array which is negative sum
-		int max_sum=Integer.MIN_VALUE;
-        int cur_sum=0;
+public class Solution {
+    public int maxSubArray(int[] A) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we will use linear complexity algorithm
         if (A==null || A.length<1)
         {
             return 0;
         }
+        int result=Integer.MIN_VALUE;
+        int cum=0;
         for (int i=0; i<A.length; i++)
         {
-            if (cur_sum<0)
+            cum+=A[i];
+            if (cum>result)
             {
-                cur_sum=0;
+                result=cum;
             }
-            cur_sum+=A[i];
-            if (cur_sum>max_sum)
+            if (cum<0)
             {
-                max_sum=cur_sum;
+                cum=0;
             }
         }
-        return max_sum;
+        return result;
     }
 }
