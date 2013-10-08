efac775c6987a6eda351e10fa57803a9695e6131
zhangtemplar <zhangtemplar@gmail.com>
2013-10-07 21:15:21 -0700

    Update ReverseInteger.java

--- src/Solution/ReverseInteger.java
+++ src/Solution/ReverseInteger.java
@@ -1,36 +1,24 @@
-package Solution;
-
-public class ReverseInteger {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public int reverse(int x) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // for negative value
+public class Solution {
+    public int reverse(int x) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // for negative number
         if (x<0)
         {
             return -reverse(-x);
         }
-        // for common case
-        // however, a potential pitfall is the overflow
-        int y=0;
-        while (x>0)
+        // we store the value as long number to avoid overflow
+        long val=x;
+        long result=0;
+        while(val>0)
         {
-            y=y*10+x%10;
-            if (y<0)
-            {
-                // System.out.println("Overflow happens");
-                return y;
-            }
-            x=x/10;
+            result=result*10+val%10;
+            val=val/10;
         }
-        return y;
+        // if overflow happens, we just cut it to the limit of the data type
+        if (result>Integer.MAX_VALUE)
+        {
+            return Integer.MAX_VALUE;
+        }
+        return (int)result;
     }
 }
