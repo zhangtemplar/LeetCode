e5145d19f24d7d74f004423d0aae8aea8807a1eb
zhangtemplar <zhangtemplar@gmail.com>
2013-10-08 20:51:14 -0700

    Update BinaryTreeInorderTraversal.java

--- src/Solution/BinaryTreeInorderTraversal.java
+++ src/Solution/BinaryTreeInorderTraversal.java
@@ -1,23 +1,16 @@
-package Solution;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Stack;
-
-public class BinaryTreeInorderTraversal {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public ArrayList<Integer> inorderTraversal(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we will stack to simulate the recursion
         ArrayList<Integer> result=new ArrayList<Integer>();
         if (root==null)
         {
@@ -26,23 +19,21 @@ public class BinaryTreeInorderTraversal {
         Stack<TreeNode> stack=new Stack<TreeNode>();
         HashSet<TreeNode> visited=new HashSet<TreeNode>();
         stack.push(root);
+        TreeNode node=null;
         while(!stack.isEmpty())
         {
-            TreeNode node=stack.pop();
+            node=stack.pop();
             if (visited.contains(node))
             {
-                // we have visisted it before
                 result.add(node.val);
             }
             else
             {
-                // first time visis
-                visited.add(node);
-                // push the left node and right node in
                 if (node.right!=null)
                 {
                     stack.push(node.right);
                 }
+                visited.add(node);
                 stack.push(node);
                 if (node.left!=null)
                 {
@@ -52,38 +43,4 @@ public class BinaryTreeInorderTraversal {
         }
         return result;
     }
-}
-
- class TreeNode
- {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode(int x) { val = x; }
-     
-     public String toString()
-     {
-    	 // use level-order traverse
-    	 StringBuffer result=new StringBuffer();
-    	 LinkedList<TreeNode> queue=new LinkedList<TreeNode>();
-    	 queue.add(this);
-    	 result.append('{');
-    	 while(!queue.isEmpty())
-    	 {
-    		 TreeNode node=queue.remove();
-    		 if (node!=null)
-    		 {
-    			 result.append(node.val);
-    			 result.append(',');
-    			 queue.add(node.left);
-    			 queue.add(node.right);
-    		 }
-    		 else
-    		 {
-    			 result.append("#,");
-    		 }
-    	 }
-    	 result.setCharAt(result.length()-1, '}');
-    	 return result.toString();
-     }
 }
