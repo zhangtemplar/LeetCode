2df688d781772b3bcbec55a0a8067a5b4fe91553
zhangtemplar <zhangtemplar@gmail.com>
2013-10-08 23:49:20 -0700

    Update SetMatrixZero.java
    Submission Result: Compile Error
    
    Line 69: not a statement [matrix[i][0]=0;][matrix[i][0]=0;)]
    
    Submission Result: Compile Error
    
    Line 79: reached end of file while parsing

--- src/Solution/SetMatrixZero.java
+++ src/Solution/SetMatrixZero.java
@@ -1,56 +1,79 @@
-package Solution;
-
-import java.util.Arrays;
-
-public class SetMatrixZero {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void setZeroes(int[][] matrix) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we set a flag to avoid duplicate operation
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // the idea is that, if column i has 0, we set matrix[0][i]=0, if row i has 0, we set matrix[i][0]=0;
+        // in addition, we use two flags to mark whether the row 0/col 0 has zero or not
+        // note that, if column i has 0, matrix[0][i] will be set to zero anyhow
         if (matrix==null || matrix.length<1 || matrix[0]==null || matrix[0].length<1)
         {
             return;
         }
-        // first scane
-        boolean[] flag_row=new boolean[matrix.length];
-        boolean[] flag_col=new boolean[matrix[0].length];
+        // we first save the first row and first column
+        boolean row_0=false;
+        for (int j=0; j<matrix[0].length; j++)
+        {
+            if (matrix[0][j]==0)
+            {
+                row_0=true;
+                break;
+            }
+        }
+        boolean col_0=false;
         for (int i=0; i<matrix.length; i++)
         {
-            // we have set this row to zero before
-            for (int j=0; j<matrix[0].length; j++)
+            if (matrix[i][0]==0)
             {
-                if(matrix[i][j]==0)
+                col_0=true;
+                break;
+            }
+        }
+        // mark the columns
+        for (int i=1; i<matrix.length; i++)
+        {
+            for (int j=1; j<matrix[i].length; j++)
+            {
+                if (matrix[i][j]==0)
                 {
-                    flag_row[i]=true;
-                    flag_col[j]=true;
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        // second scan
-        for (int i=0; i<flag_row.length; i++)
+        // set the rows
+        for (int i=1; i<matrix.length; i++)
         {
-            if (flag_row[i])
+            if (matrix[i][0]==0)
             {
-                Arrays.fill(matrix[i], 0);
-            }
-        }
-        for (int j=0; j<flag_col.length; j++)
-        {
-            if (flag_col[j])
-            {
-                for (int i=0; i<flag_row.length; i++)
+                for (int j=1; j<matrix[i].length; j++)
                 {
                     matrix[i][j]=0;
                 }
+            }
+        }
+        // set the columns
+        for (int j=1; j<matrix[0].length; j++)
+        {
+            if (matrix[0][j]==0)
+            {
+                for (int i=1; i<matrix.length; i++)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        // for the first row and column
+        if (row_0==true)
+        {
+            for (int j=0; j<matrix[0].length; j++)
+            {
+                matrix[0][j]=0;
+            }
+        }
+        if (col_0==true)
+        {
+            for (int i=0; i<matrix.length; i++)
+            {
+                matrix[i][0]=0;
             }
         }
     }
