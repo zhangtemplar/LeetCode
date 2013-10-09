66b5ff8f15aa59de77a70f1a0e825935775524bb
zhangtemplar <zhangtemplar@gmail.com>
2013-10-08 22:28:14 -0700

    Update RotateImage.java
    Submission Result: Runtime Error
    
    Last executed input:	[[1,2],[3,4]]
    
    Submission Result: Wrong Answer
    
    Input:	[[1,2,3],[4,5,6],[7,8,9]]
    Output:	[[7,2,1],[4,5,6],[9,8,3]]
    Expected:	[[7,4,1],[8,5,2],[9,6,3]]

--- src/Solution/RotateImage.java
+++ src/Solution/RotateImage.java
@@ -1,25 +1,16 @@
-package Solution;
-
-public class RotateImage {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void rotate(int[][] matrix) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
+public class Solution {
+    public void rotate(int[][] matrix) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // the function is [x,y]->[n-y,x]
         if (matrix==null || matrix.length<1)
         {
             return;
         }
-        int a, b, c, d;
         int n=matrix.length;
-        // we only need to process quater of the matrix
+        int a=0;
+        int b=0;
+        int c=0;
+        int d=0;
         for (int i=0; i<(n+1)/2; i++)
         {
             for (int j=0; j<n/2; j++)
@@ -28,10 +19,10 @@ public class RotateImage {
                 b=matrix[j][n-i-1];
                 c=matrix[n-i-1][n-j-1];
                 d=matrix[n-j-1][i];
+                matrix[i][j]=d;
                 matrix[j][n-i-1]=a;
                 matrix[n-i-1][n-j-1]=b;
                 matrix[n-j-1][i]=c;
-                matrix[i][j]=d;
             }
         }
     }
