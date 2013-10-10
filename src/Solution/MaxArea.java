36298db97dd3ff4573b59e37a5c46826e56ec170
zhangtemplar <zhangtemplar@gmail.com>
2013-10-09 20:50:17 -0700

    Update MaxArea.java

--- src/Solution/MaxArea.java
+++ src/Solution/MaxArea.java
@@ -1,62 +1,33 @@
-package Solution;
-
-public class MaxArea {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
-	
-	public static int maxArea(int[] height) {
-		// Start typing your Java solution below
-        // DO NOT write main() function
+public class Solution {
+    public int maxArea(int[] height) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // with exhaustive search, we have an algorithm of square complexity
+        // we should have a linear algorithm
         if (height==null || height.length<2)
         {
             return 0;
         }
-        // we search for each pair of data points and keep the maximal
-        // this algorithm has complexity O(n^2)
-        /*
-        int volume=0;
-        for (int i=0; i<height.length; i++)
-        {
-            for (int j=height.length-1; j>i; j--)
-            {
-                int val=(j-i)*(height[i]>height[j]?height[j]:height[i]);
-                if (val>volume)
-                {
-                    volume=val;
-                }
-            }
-        }
-        */
-        // try a new one
-        // this one should be have complexity O(n)
-        int volume=0;
-        int i, j;
-        i=0;
-        j=height.length-1;
+        int result=0;
+        // we will search from both left and right
+        // if the a[left]<a[right], the only possbility of better result is left++, otherwise right--
+        int i=0;
+        int j=height.length-1;
         while(i<j)
         {
-            int val=(j-i)*(height[i]>height[j]?height[j]:height[i]);
-            if (val>volume)
+            int volume=(j-i)*(height[i]<height[j]?height[i]:height[j]);
+            if (result<volume)
             {
-                volume=val;
+                result=volume;
             }
-            if (height[j]<height[i])
+            if (height[i]<height[j])
             {
-                // since height[j] is the shorter one, so our best chance is 
-                // try the other one to increase it
-                j--;
+                i++;
             }
             else
             {
-                // all other case
-                i++;
+                j--;
             }
         }
-        return volume;
+        return result;
     }
 }
