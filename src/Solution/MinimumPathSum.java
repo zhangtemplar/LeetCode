47d7806a2f2d7c38698863842e769cbf2868660d
zhangtemplar <zhangtemplar@gmail.com>
2013-10-09 21:49:26 -0700

    Update MinimumPathSum.java

--- src/Solution/MinimumPathSum.java
+++ src/Solution/MinimumPathSum.java
@@ -1,46 +1,33 @@
-package Solution;
-
-public class MinimumPathSum {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public int minPathSum(int[][] grid) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // this is the minimum path problem for the graph
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we can use dynamic programming
+        // buffer[i][j] is the sum of the minimum path from [0][0]
+        // obviously, buffer[i][j]=min(buffer[i-1][j],buffer[i][j-1])+grid[i][j]
         if (grid==null || grid.length<1 || grid[0]==null || grid[0].length<1)
         {
             return 0;
         }
-        int m=grid.length;
-        int n=grid[0].length;
-        int [][]result=new int[m][n];
-        if(m==1 && n==1)
+        int [][]buffer=new int[grid.length][grid[0].length];
+        int result=0;
+        // initialization
+        buffer[0][0]=grid[0][0];
+        for (int i=1; i<grid.length; i++)
         {
-            return grid[0][0];
+            buffer[i][0]=buffer[i-1][0]+grid[i][0];
         }
-        result[0][0]=grid[0][0];
-        for (int i=1; i<m; i++)
+        for (int j=1; j<grid[0].length; j++)
         {
-            result[i][0]=result[i-1][0]+grid[i][0];
+            buffer[0][j]=buffer[0][j-1]+grid[0][j];
         }
-        for (int j=1; j<n; j++)
+        // progress
+        for (int i=1; i<grid.length; i++)
         {
-            result[0][j]=result[0][j-1]+grid[0][j];
+            for (int j=1; j<grid[i].length; j++)
+            {
+                buffer[i][j]=grid[i][j]+(buffer[i-1][j]<buffer[i][j-1]?buffer[i-1][j]:buffer[i][j-1]);
+            }
         }
-        for (int i=1; i<m; i++)
-        {
-             for (int j=1; j<n; j++)
-             {
-                 result[i][j]=(result[i-1][j]>result[i][j-1]?result[i][j-1]:result[i-1][j])+grid[i][j];
-             }
-        }
-        return result[m-1][n-1];
+        return buffer[buffer.length-1][buffer[buffer.length-1].length-1];
     }
 }
