91abfa0bcd16a2402cd66b7231e9681a4ccd4703
zhangtemplar <zhangtemplar@gmail.com>
2013-10-09 21:40:16 -0700

    Update PlusOne.java
    Submission Result: Wrong Answer
    
    Input:	[0]
    Output:	[0]
    Expected:	[1]

--- src/Solution/PlusOne.java
+++ src/Solution/PlusOne.java
@@ -1,125 +1,27 @@
-package Solution;
-
-import java.util.Arrays;
-
-public class PlusOne {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public int[] plusOne(int[] digits) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // note that, the size of the output could be one more than the input
+        // we assume the number is stored such that higher bit is stored first
         int []result;
         if (digits==null || digits.length<1)
         {
             result=new int[1];
             result[0]=1;
-            return result;
         }
-        if (digits[0]<0)
-        {
-            // for the sign
-            result=minusOne(digits);
-            result[0]=-result[0];
-            return result;
-        }
-        else
-        {
-            // for the main
-            int increment=1;
-            for (int i=digits.length-1; i>=0; i--)
-            {
-                increment+=digits[i];
-                digits[i]=increment%10;
-                increment=increment/10;
-            }
-            if (increment>0)
-            {
-                // we have an extra bit
-                // we need to remove the zeros at the beginning
-                result=new int[digits.length+1];
-                result[0]=increment;
-                for (int j=1; j<result.length; j++)
-                {
-                    result[j]=digits[j-1];
-                }
-            }
-            else
-            {
-                // we have an extra bit
-                // we need to remove the zeros at the beginning
-                int i;
-                for (i=0; i<digits.length; i++)
-                {
-                    if (digits[i]>0)
-                    {
-                        break;
-                    }
-                }
-                if (i>=digits.length)
-                {
-                    // the result is simply zero
-                    result=new int[1];
-                }
-                else
-                {
-                    result=Arrays.copyOfRange(digits, i, digits.length);
-                }
-            }
-            return result;
-        }
-    }
-    
-    private int[] minusOne(int[] digits)
-    {
-        // for the main
-        int []result=null;
-        int increment=-1;
+        result=new int[digits.length];
+        int increment=1;
         for (int i=digits.length-1; i>=0; i--)
         {
-            if (digits[i]+increment<0)
-            {
-                digits[i]=10+increment;
-            }
-            else
-            {
-                digits[i]-=increment;
-                increment=0;
-            }
+            int val=digits[i]+increment;
+            result[i]=val%10;
+            increment=val/10;
         }
-        if (increment<0)
+        if (increment!=0)
         {
-            // the only possbility here is that, the number is too small
-            // however this is impossible: to have this happen digits=0
-            // but it will never be passed to here
-        }
-        else
-        {
-            // we have an extra bit
-            // we need to remove the zeros at the beginning
-            int i;
-            for (i=0; i<digits.length; i++)
-            {
-                if (digits[i]>0)
-                {
-                    break;
-                }
-            }
-            if (i>=digits.length)
-            {
-                // the result is simply zero
-                result=new int[1];
-            }
-            else
-            {
-                result=Arrays.copyOfRange(digits, i, digits.length);
-            }
+            // we have an extra digit to fit in
+            result=new int[digits.length+1];
+            result[0]=1;
         }
         return result;
     }
