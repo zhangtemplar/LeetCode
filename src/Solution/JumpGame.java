fabd1e0b398ca4c50a93f4bf8adc2fef2fa8537b
zhangtemplar <zhangtemplar@gmail.com>
2013-10-10 23:23:35 -0700

    Update JumpGame.java

--- src/Solution/JumpGame.java
+++ src/Solution/JumpGame.java
@@ -1,27 +1,26 @@
-package Solution;
-
-public class JumpGame {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public boolean canJump(int[] A) 
-	{
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we will use depth first search for this problem
-        int maxCover = 0;  
-        for(int start =0; start<= maxCover && start<A.length; start++)  
-        {  
-            if(A[start]+start > maxCover)  
-                maxCover = A[start]+start;  
-                if(maxCover >= A.length-1) return true;  
-        }  
-        return false;  
+public class Solution {
+    public boolean canJump(int[] A) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // dynamic programming
+        // f[i] the remaining steps when we go from 0 to i. If f[i]<0, we can't even make to i
+        // f[i]=0, i=0;
+        // f[i]=max(f[i-1], A[i-1])-1
+        // from 0 to i-1, we have two choice: we first go to i-1, then move to i; or we move from
+        // some location which lead to i-1.
+        if (A==null || A.length<1)
+        {
+            return true;
+        }
+        int []buffer=new int[A.length];
+        buffer[0]=0;
+        for (int i=1; i<A.length; i++)
+        {
+            buffer[i]=Math.max(buffer[i-1],A[i-1])-1;
+            if (buffer[i]<0)
+            {
+                return false;
+            }
+        }
+        return buffer[A.length-1]>=0;
     }
 }
