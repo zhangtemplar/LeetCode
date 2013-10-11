d6bcaf91b2e969e0650f2b1b7ec40cd9eed6e77e
zhangtemplar <zhangtemplar@gmail.com>
2013-10-10 23:56:43 -0700

    Update SearchRange.java
    Submission Result: Compile Error
    
    Line 77: missing return statement
    
    Submission Result: Wrong Answer
    
    Input:	[2,2], 1
    Output:	[0,0]
    Expected:	[-1,-1]

--- src/Solution/SearchRange.java
+++ src/Solution/SearchRange.java
@@ -1,24 +1,11 @@
-package Solution;
-
-public class SearchRange {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public int[] searchRange(int[] A, int target) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we will do it similarly as binary search, however,
-        // instead of direct return when getting search result
-        // we need to wait for the other results
+public class Solution {
+    public int[] searchRange(int[] A, int target) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // binary search
+        // the only different is that, when we find a solution, we don't immediately stop the algorithm
+        int []result=new int[2];
         if (A==null || A.length<1)
         {
-            int[] result=new int[2];
             result[0]=-1;
             result[1]=-1;
             return result;
@@ -28,53 +15,67 @@ public class SearchRange {
     
     private int[] searchRange(int[] A, int start, int end, int target)
     {
-        // basic case
-        int[] result=new int[2];
+        int []result=new int[2];
         result[0]=-1;
         result[1]=-1;
-        if (start>end)
-        {
-            return result;
-        }
+        // base case
         if (start==end)
         {
-            if (A[start]==target)
+            if (A[start]!=target)
+            {
+                result[0]=-1;
+                result[1]=-1;
+            }
+            else
             {
                 result[0]=start;
-                result[1]=start;
+                result[1]=end;
             }
-            return result;
-        }
-        int middle=(start+end)/2;
-        // search for the left
-        int[] result_left=new int[2];
-        result_left[0]=-1;
-        result_left[1]=-1;
-        if (A[middle]>=target && A[start]<=target)
-        {
-            result_left=searchRange(A, start, middle, target);
-        }
-        // for the right
-        int[] result_right=new int[2];
-        result_right[0]=-1;
-        result_right[1]=-1;
-        if (A[middle+1]<=target && A[end]>=target)
-        {
-            result_right=searchRange(A, middle+1, end, target);
-        }
-        // merge the result
-        if (result_left[1]>-1 && result_right[0]>-1)
-        {
-            result_left[1]=result_right[1];
-            return result_left;
-        }
-        else if (result_left[1]>-1)
-        {
-            return result_left;
         }
         else
         {
-            return result_right;
+            int middle=(start+end)/2;
+            if (A[middle]==target)
+            {
+                result[0]=middle;
+                result[1]=middle;
+            }
+            // in the right half
+            int []result_right=new int[2];
+            result_right[0]=-1;
+            result_right[1]=-1;
+            if (A[middle]<=target)
+            {
+                result_right=searchRange(A, middle+1, end, target);
+            }
+            // in the left half
+            int []result_left=new int[2];
+            result_left[0]=-1;
+            result_left[1]=-1;
+            if (A[middle]>=target && middle>start)
+            {
+                result_left=searchRange(A, start, middle-1, target);
+            }
+            // merge the result
+            // left boundary
+            if (result_left[0]>=0)
+            {
+                result[0]=result_left[0];
+            }
+            else if(result[0]<0)
+            {
+                result[0]=result_right[0];
+            }
+            // right boundary
+            if (result_right[1]>=0)
+            {
+                result[1]=result_right[1];
+            }
+            else if(result[1]<0)
+            {
+                result[1]=result_left[1];
+            }
         }
+        return result;
     }
 }
