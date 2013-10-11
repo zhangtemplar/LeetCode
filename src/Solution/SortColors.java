9de30f8d4c2b983151e64422aa05f4cb1cca5944
zhangtemplar <zhangtemplar@gmail.com>
2013-10-10 21:19:54 -0700

    Update SortColors.java
    Submission Result: Compile Error
    
    Line 25: ';' expected
    
    Submission Result: Compile Error
    
    Line 37: ';' expected
    
    Submission Result: Wrong Answer
    
    Input:	[1,1,2]
    Output:	[1,2,1]
    Expected:	[1,1,2]
    
    
    Submission Result: Wrong Answer L 20
    
    Input:	[1,2,0]
    Output:	[1,2,0]
    Expected:	[0,1,2]
    
    
    Submission Result: Wrong Answer
    
    Input:	[2,0]
    Output:	[2,0]
    Expected:	[0,2]

--- src/Solution/SortColors.java
+++ src/Solution/SortColors.java
@@ -1,32 +1,47 @@
-package Solution;
-
-import java.util.Arrays;
-
-public class SortColors {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void sortColors(int[] A) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we just count how many objects are for each color
+public class Solution {
+    public void sortColors(int[] A) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // our algorithm is follows: we scan the array, whenever we met 0, we swap it with the first element
+        // which is not zero (maintained by an index); or we met 2, we swap it the last element which is not 2.
+        // After that, all 0 must be in the front of the array and 2 will be in the end of array
         if (A==null || A.length<1)
         {
             return;
         }
-        int []count=new int[3];
-        for (int i=0; i<A.length; i++)
+        int first0=0;
+        int last2=A.length-1;
+        int swap=0;
+        for(int i=0; i<A.length && first0<=last2 && i<=last2 && i>=first0; i++)
         {
-            count[A[i]]++;
+            // for 0, we make it to the front
+            if (A[i]==0)
+            {
+                // swap it to the front
+                while (A[first0]==0 && first0<i && first0<A.length-1)
+                {
+                    first0++;
+                }
+                swap=A[i];
+                A[i]=A[first0];
+                A[first0++]=swap;
+                // note that, only 1 will be swaped back
+            }
+            // for 2, we make it to the end
+            else if(A[i]==2)
+            {
+                while (A[last2]==2 && last2>i && last2>0)
+                {
+                    last2--;
+                }
+                swap=A[i];
+                A[i]=A[last2];
+                A[last2--]=swap;
+                // in case, 0 is swapped ahead
+                if (A[i]==0)
+                {
+                    i--;
+                }
+            }
         }
-        Arrays.fill(A, 0, count[0], 0);
-        Arrays.fill(A, count[0], count[1]+count[0], 1);
-        Arrays.fill(A, count[0]+count[1], count[2]+count[1]+count[0], 2);
     }
 }
