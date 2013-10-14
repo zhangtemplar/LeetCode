9741e2ee027fd32f23429c9bf21b7321d2648580
zhangtemplar <zhangtemplar@gmail.com>
2013-10-13 23:18:44 -0700

    Update PathSumII.java
    Submission Result: Compile Error
    
    Line 37: not a statement
    
    Submission Result: Compile Error
    
    Line 35: cannot find symbol: method SubPathSum(TreeNode,int)

--- src/Solution/PathSumII.java
+++ src/Solution/PathSumII.java
@@ -1,58 +1,57 @@
-package Solution;
-
-import java.util.ArrayList;
-
-public class PathSumII {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        return pathSum(root, sum, new ArrayList<Integer>());
-    }
-    
-    // path is the current partial path
-    private ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum, ArrayList<Integer> path)
-    {
-        ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>();
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we will use recursion, for each node, we store the sum-(sum of value from root to this node).
+        // if we find a leaf node and this value is zero, we add the path
         if (root==null)
         {
-            return result;
+            return new ArrayList<ArrayList<Integer>>();
         }
-        sum=sum-root.val;
-        ArrayList<Integer> new_path=new ArrayList<Integer>(path);
-        new_path.add(root.val);
-        // this is a leaf node
+        sum-=root.val;
+        // leaf node, check whether this is a valid path
+        ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>();
         if (root.left==null && root.right==null)
         {
+            // yes, this is a valid path
             if (sum==0)
             {
-                result.add(new_path);
+                ArrayList<Integer> path=new ArrayList<Integer>();
+                path.add(root.val);
+                result.add(path);
             }
-            return result;
         }
-        // otherwise check it children
+        // nonleaf node, we need to check the two subtree
         if (root.left!=null)
         {
-            ArrayList<ArrayList<Integer>> base=pathSum(root.left, sum, new_path);
+            ArrayList<ArrayList<Integer>> base=pathSum(root.left, sum);
+            // check the path
             for (ArrayList<Integer> entry: base)
             {
-                result.add(entry);
+                ArrayList<Integer> path=new ArrayList<Integer>();
+                path.add(root.val);
+                path.addAll(entry);
+                result.add(path);
             }
         }
         if (root.right!=null)
         {
-            ArrayList<ArrayList<Integer>> base=pathSum(root.right, sum, new_path);
+            ArrayList<ArrayList<Integer>> base=pathSum(root.right, sum);
+            // check the path
             for (ArrayList<Integer> entry: base)
             {
-                result.add(entry);
+                ArrayList<Integer> path=new ArrayList<Integer>();
+                path.add(root.val);
+                path.addAll(entry);
+                result.add(path);
             }
         }
         return result;
