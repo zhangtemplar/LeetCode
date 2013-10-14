d08820b1ec71a0ee552650c1b098ffd4968acbd1
zhangtemplar <zhangtemplar@gmail.com>
2013-10-14 00:07:56 -0700

    Update Triangle.java
    Submission Result: Runtime Error
    
    Last executed input:	[[-1],[2,3],[1,-1,-3]]

--- src/Solution/Triangle.java
+++ src/Solution/Triangle.java
@@ -1,42 +1,29 @@
-package Solution;
-
-import java.util.ArrayList;
-
-public class Triangle {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // the algorithm is easy
-        ArrayList<Integer> prev=new ArrayList<Integer>();
-        ArrayList<Integer> current=new ArrayList<Integer>();
-        if (triangle==null || triangle.size()<1)
+public class Solution {
+    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // # rows = max # column
+        // we will process from top to bottom, compute the sum of minimum path from top to the current position
+        if (triangle==null)
         {
             return 0;
         }
-        current.add(triangle.get(0).get(0));
-        for (int l=1; l<triangle.size(); l++)
+        ArrayList<Integer> prev=new ArrayList<Integer>();
+        ArrayList<Integer> current=null;
+        prev.add(triangle.get(0).get(0));
+        for (int i=1; i<triangle.size(); i++)
         {
-            prev=current;
             current=new ArrayList<Integer>();
-            current.add(prev.get(0)+triangle.get(l).get(0));
-            for (int i=1; i<prev.size(); i++)
+            current.add(triangle.get(i).get(0)+prev.get(0));
+            for (int j=1; j<triangle.get(i).size()-1; j++)
             {
-                current.add(Math.min(prev.get(i-1), prev.get(i))+triangle.get(l).get(i));
+                current.add(Math.min(prev.get(j), prev.get(j-1))+triangle.get(i).get(j));
             }
-            current.add(prev.get(prev.size()-1)+triangle.get(l).get(triangle.get(l).size()-1));
+            current.add(triangle.get(i).get(triangle.get(i).size()-1)+prev.get(prev.size()-1));
+            prev=current;
         }
-        // find the min of the last row
+        // find the minimal
         int min=Integer.MAX_VALUE;
-        for (int x: current)
+        for (int x: prev)
         {
             if (min>x)
             {
