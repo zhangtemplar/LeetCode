a46d539e306bc5b10ba64f28c6765930335991ed
zhangtemplar <zhangtemplar@gmail.com>
2013-10-13 22:39:21 -0700

    Update SubsetsII.java

--- src/Solution/SubsetsII.java
+++ src/Solution/SubsetsII.java
@@ -1,62 +1,43 @@
-package Solution;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-
-public class SubsetsII {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we still use recursion
-        // subsets(S[0:n])=S[n-1] U {subsets(S[0:n-1] x S[n-1]} U subsets(S[0:n-1]
+public class Solution {
+    public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we shall use recursion
+        // Say s(A[1: n]) is the subset of element with A[1: n], then the subset with {A[1: n] U B}
+        // would be S(A[1: n]) U B U (S(A[1: n]xB)
         if (num==null || num.length<1)
         {
             return new ArrayList<ArrayList<Integer>>();
         }
+        // sort
         Arrays.sort(num);
-        return new ArrayList<ArrayList<Integer>>(subsetsWithDup(num, num.length-1));
+        return subsetsWithDup(num, num.length-1);
     }
     
-    private HashSet<ArrayList<Integer>> subsetsWithDup(int[] S, int n)
+    private ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num, int end)
     {
-        // the algorithm is the similar as the previous one
-        // except that, we will need hashset instead of ArrayList to reove duplicate
-        HashSet<ArrayList<Integer>> result=new HashSet<ArrayList<Integer>>();
-        if (n<0)
+        ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>();
+        if (end==0)
         {
             ArrayList<Integer> entry=new ArrayList<Integer>();
+            entry.add(num[end]);
             result.add(entry);
-            return result;
+            entry=new ArrayList<Integer>();
+            result.add(entry);
         }
         else
         {
-            HashSet<ArrayList<Integer>> base=subsetsWithDup(S, n-1);
-            for (ArrayList<Integer> entry: base)
+            ArrayList<ArrayList<Integer>> base=subsetsWithDup(num, end-1);
+            // we will use hashset to remove the duplicate
+            HashSet<ArrayList<Integer>> set=new HashSet<ArrayList<Integer>>();
+            for (ArrayList<Integer> element: base)
             {
-                // we don't need to consider the duplicate here
-                result.add(entry);
+                set.add(element);
+                ArrayList<Integer> entry=new ArrayList<Integer>(element);
+                entry.add(num[end]);
+                set.add(entry);
             }
-            for (ArrayList<Integer> entry: base)
-            {
-                ArrayList<Integer> new_entry=new ArrayList<Integer>(entry);
-                new_entry.add(S[n]);
-                // check the duplication
-                if (!result.contains(new_entry))
-                {
-                    result.add(new_entry);
-                }
-            }
-            return result;
+            result.addAll(set);
         }
+        return result;
     }
 }
