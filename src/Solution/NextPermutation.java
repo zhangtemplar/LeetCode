871e4e5df2b1230b31e3354522ac2ffd50197787
zhangtemplar <zhangtemplar@gmail.com>
2013-10-15 00:09:39 -0700

    Update NextPermutation.java
    Submission Result: Runtime Error
    
    Last executed input:	[1,2,0,3,0,1,2,4]
     L 31

--- src/Solution/NextPermutation.java
+++ src/Solution/NextPermutation.java
@@ -1,75 +1,40 @@
-package Solution;
-
-public class NextPermutation {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		NextPermutation instance=new NextPermutation();
-		int []num={1,1};
-		instance.nextPermutation(num);
-		System.out.println(num);
-	}
-
-	public void nextPermutation(int[] num) {
-		// Start typing your Java solution below
-        // DO NOT write main() function
+public class Solution {
+    public void nextPermutation(int[] num) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we find the rightmost element, which is smaller than its next right element
+        // if this element dosen't exist, it means the arrangement is no possible
         if (num==null || num.length<2)
         {
             return;
         }
-        // Step 1 from right to left, find the first digit which is smaller than its left
-        int separator=0;
-        for (int i=num.length-1; i>0; i--)
+        int target=0;
+        for (target=num.length-2; target>=0; target--)
         {
-            if (num[i]>num[i-1])
+            if(num[target]<num[target+1])
             {
-                separator=i;
                 break;
             }
         }
-        int swap;
-        // this number could be already the largest, we return it as the smallest
-        if (separator<1)
+        // such arrangement is not possible, we just sort the array in ascending order
+        if (target<0)
         {
-            for (int i=0; i<num.length-1-i; i++)
-            {
-                swap=num[i];
-                num[i]=num[num.length-1-i];
-                num[num.length-1-i]=swap;
-            }
+            Arrays.sort(num);
             return;
         }
-        // Step 2 & Step 3
-        // after sorting, the smallest digit required in Step 2, will be on arr[i-1]
-        // since the number of digit is limited a bubble sorting is not that bad
-        for (int i=separator; i<num.length; i++)
+        // otherwise, we do the following
+        // then sort from target+1
+        Arrays.sort(num, target+1, num.length);
+        // swap the target with the smallest element larger than it
+        int i=0;
+        for (i=target+1; i<num.length; i++)
         {
-            for (int j=i+1; j<num.length; j++)
+            if (num[i]>num[target])
             {
-                if (num[i]>num[j])
-                {
-                    swap=num[i];
-                    num[i]=num[j];
-                    num[j]=swap;
-                }
-            }
-        }
-        // final step
-        // find the smallest element that is larger num[sepeator-1]
-        int min_index=num.length-1;
-        for (int i=separator; i<num.length-1; i++)
-        {
-            if (num[i]>num[separator-1])
-            {
-                min_index=i;
                 break;
             }
         }
-        swap=num[separator-1];
-        num[separator-1]=num[min_index];
-        num[min_index]=swap;
+        int swap=num[target];
+        num[target]=num[i];
+        num[i]=swap;
     }
 }
