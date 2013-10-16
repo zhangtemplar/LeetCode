831f06b2d3e19e920d5f680e1dd002a6bc5da1e7
zhangtemplar <zhangtemplar@gmail.com>
2013-10-15 22:25:47 -0700

    Update Pow.java

--- src/Solution/Pow.java
+++ src/Solution/Pow.java
@@ -1,37 +1,23 @@
-package Solution;
-
-public class Pow {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public double pow(double x, int n) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we use recursion to do that
-        // x^n=(x^n/2)^2*x^(n-n/2*2)
-        // we will need to take care of the very small number
+public class Solution {
+    public double pow(double x, int n) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // O(log(n))
         if (n==0)
         {
             return 1;
         }
-        else if (n==-1)
-        {
-            return 1/x;
-        }
-        if (n==1)
+        else if(n==1)
         {
             return x;
+        }
+        else if(n==-1)
+        {
+            return 1/x;
         }
         else
         {
             double y=pow(x, n/2);
-            return y*y*pow(x, n-n/2*2);
+            return y*y*pow(x, n-(n/2)*2);
         }
     }
 }
