77958573d50f8f4a65ce981122e3f8ae630e5fed
zhangtemplar <zhangtemplar@gmail.com>
2013-10-15 22:47:17 -0700

    Update ConvertSortedListToBinarySearchTree.java
    Submission Result: Compile Error
    
    Line 46: illegal start of expression
    
    Submission Result: Runtime Error (L 53 : L 56)
    
    Last executed input:	{-1,0,1,2}

--- src/Solution/ConvertSortedListToBinarySearchTree.java
+++ src/Solution/ConvertSortedListToBinarySearchTree.java
@@ -1,59 +1,60 @@
-package Solution;
-
-public class ConvertSortedListToBinarySearchTree {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public TreeNode sortedListToBST(ListNode head) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // the point is how to find the middle point quickly
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        ListNode tail=head;
+        while(tail!=null && tail.next!=null)
+        {
+            tail=tail.next;
+        }
+        return sortedListToBST(head, tail);
+    }
+    
+    private TreeNode sortedListToBST(ListNode head, ListNode tail)
+    {
+        // to convert an sorted array to a BST, we always use middle as root and iterate this step
+        // so the problem is how to find the middle fast
         if (head==null)
         {
             return null;
         }
-        ListNode middle=findMiddle(head, null);
-        TreeNode root=new TreeNode(middle.val);
-        root.left=sortedListToBST(head, middle);
-        root.right=sortedListToBST(middle.next, null);
-        return root;
-    }
-    
-    // build a subtree for list [start, end), i.e., end is exclusive
-    public TreeNode sortedListToBST(ListNode start, ListNode end)
-    {
-        if (start==null || start==end)
+        else if(head==tail)
         {
-            return null;
+            return new TreeNode(head.val);
         }
-        ListNode middle=findMiddle(start, end);
-        TreeNode root=new TreeNode(middle.val);
-        if (start.next==end)
+        else
         {
+            // find the previous of middle, note, the left half could have one more node than the right half
+            ListNode middle=head;
+            ListNode end=head;
+            while(end!=null && end.next!=tail && end.next!=null && end.next.next!=tail)
+            {
+                middle=middle.next;
+                end=end.next.next;
+            }
+            TreeNode root=new TreeNode(middle.next.val);
+            root.left=sortedListToBST(head, middle);
+            if(head.next!=tail)
+            {
+            	root.right=sortedListToBST(middle.next.next, tail);
+            }
             return root;
         }
-        root.left=sortedListToBST(start, middle);
-        root.right=sortedListToBST(middle.next, end);
-        return root;
-    }
-    
-    // end is exclusive
-    public ListNode findMiddle(ListNode start, ListNode end)
-    {
-        ListNode middle, terminate;
-        middle=start;
-        terminate=start;
-        while(terminate!=null && terminate.next!=null && terminate!=end && terminate.next!=end)
-        {
-            middle=middle.next;
-            terminate=terminate.next.next;
-        }
-        return middle;
     }
 }
