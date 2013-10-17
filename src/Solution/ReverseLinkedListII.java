586580280055b01c6c84926553a7509dbfd51776
zhangtemplar <zhangtemplar@gmail.com>
2013-10-17 00:16:03 -0700

    Update ReverseLinkedListII.java

--- src/Solution/ReverseLinkedListII.java
+++ src/Solution/ReverseLinkedListII.java
@@ -1,61 +1,55 @@
-package Solution;
-
-public class ReverseLinkedListII {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ReverseLinkedListII instance=new ReverseLinkedListII();
-		ListNode head=new ListNode(1);
-		head.next=new ListNode(2);
-		head.next.next=new ListNode(3);
-		ListNode result=instance.reverseBetween(head, 1, 3);
-		System.out.println(instance.printListNode(result));
-	}
-
-	public String printListNode(ListNode head)
-	{
-		StringBuffer result=new StringBuffer();
-		while(head!=null)
-		{
-			result.append(head.val+"->");
-			head=head.next;
-		}
-		return result.toString();
-	}
-	
-	public ListNode reverseBetween(ListNode head, int m, int n) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // first find that node
-        // 1) the node right before the m_th node
-        // 2) the current node we are processing
-        // 3) the n_th node
-        int k=1;
-        ListNode prev, current, last;
-        // create an axulliar node as the new head
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we first create a fake head
+        if (head==null || head.next==null)
+        {
+            return head;
+        }
         ListNode result=new ListNode(0);
         result.next=head;
+        // we can use a stack for this purpose
+        int i=1;
         ListNode node=result;
-        while(k<m && node!=null)
+        while(i<m && node!=null)
         {
             node=node.next;
-            k++;
+            i++;
         }
-        // now we have prev.next=m_th node
-        prev=node;
-        current=prev.next;
-        while(k<n && current.next!=null)
+        ListNode start=node;
+        // it is not long enough
+        if (node==null)
         {
-        	node=prev.next;
-            prev.next=current.next;
-            last=current.next;
-            current.next=last.next;
-            last.next=node;
-            k++;
+            return head;
         }
+        node=node.next;
+        // record the list to be reversed
+        Stack<ListNode> stack=new Stack<ListNode>();
+        while(i<=n && node!=null)
+        {
+            stack.push(node);
+            node=node.next;
+            i++;
+        }
+        ListNode end=node;
+        // pop out the node
+        while(!stack.isEmpty())
+        {
+            start.next=stack.pop();
+            start=start.next;
+        }
+        start.next=end;
         return result.next;
     }
 }
