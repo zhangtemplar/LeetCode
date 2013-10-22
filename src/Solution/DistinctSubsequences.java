b69484de80f256f4cb37dde09dc9be7ab097693f
zhangtemplar <zhangtemplar@gmail.com>
2013-10-22 00:43:45 -0700

    Update DistinctSubsequences.java
    Submission Result: Compile Error
    
    Line 25: cannot find symbol: variable f

--- src/Solution/DistinctSubsequences.java
+++ src/Solution/DistinctSubsequences.java
@@ -1,134 +1,55 @@
-package Solution;
-
-public class DistinctSubsequences {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		DistinctSubsequences instance=new DistinctSubsequences();
-		System.out.println(instance.numDistinct("aabdbaabeeadcbbdedacbbeecbabebaeeecaeabaedadcbdbcdaabebdadbbaeabdadeaabbabbecebbebcaddaacccebeaeedababedeacdeaaaeeaecbe", "bddabdcae"));
-	}
-
-	public int numDistinct(String S, String T) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we will use dynamic programming
-        if (S==null || S.length()<1)
-        {
-             if (T==null || T.length()<1)
-             {
-                 return 1;
-             }
-             else
-             {
-                 return 0;
-             }
-        }
-        else if (T==null || T.length()<1)
-        {
-            return 1;
-        }
-        else if (S.length()<T.length())
+public class Solution {
+    public int numDistinct(String S, String T) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we will use recursion
+        if (S==null || T==null || S.length()<T.length())
         {
             return 0;
         }
-        // result[i][j] is the # of such match for S[0:i] and T[0:j]
-        int [][]result=new int[S.length()][T.length()];
-        if (S.charAt(0)==T.charAt(0))
+        // we could use a dynamic programming
+        // -1 means things haven't explored
+        int[][] buffer=new int[S.length()+1][T.length()+1];
+        // initialization
+        // success case
+        for (int i=0; i<=S.length(); i++)
         {
-            result[0][0]=1;
+            buffer[i][T.length()]=1;
         }
-        else
+        // progress
+        for (int i=S.length()-1; i>=0; i--)
         {
-            result[0][0]=0;
-        }
-        for (int j=1; j<T.length(); j++)
-        {
-            result[0][j]=0;
-        }
-        for (int i=1; i<S.length(); i++)
-        {
-            if (S.charAt(i)==T.charAt(0))
-            {
-                result[i][0]=result[i-1][0]+1;
-            }
-            else
-            {
-                result[i][0]=result[i-1][0];
-            }
-        }
-        for (int i=1; i<S.length(); i++)
-        {
-            for (int j=1; j<T.length(); j++)
+            for (int j=T.length()-1; j>=0; j--)
             {
                 if (S.charAt(i)==T.charAt(j))
                 {
-                    result[i][j]=result[i-1][j]+result[i-1][j-1];
+                    buffer[i][j]=buffer[i+1][j+1]+buffer[i+1][j];
                 }
                 else
                 {
-                    result[i][j]=result[i-1][j];
+                    buffer[i][j]=buffer[i+1][j];
                 }
             }
         }
-        return result[S.length()-1][T.length()-1];
+        return buffer[0][0];
     }
-	
-	public int numDistinctSlow(String S, String T) {
-		// Start typing your Java solution below
-        // DO NOT write main() function
-        // we will use recursion
-        // in each level, we try to remove one of the letter from S, until 
-        // S is the same long as T
-        // Note that, to avoid duplicate, we only remove the letter which is
-        // after the removal of previous step
-        if (S==null || S.length()<1)
-        {
-             if (T==null || T.length()<1)
-             {
-                 return 1;
-             }
-             else
-             {
-                 return 0;
-             }
-        }
-        else if (T==null || T.length()<1)
+
+    private int numDistinct(String S, int i, String T, int j)
+    {
+        if (j>=T.length())
         {
             return 1;
         }
-        else if (S.length()<T.length())
+        if (i>=S.length() || S.length()-i<T.length()-j)
         {
             return 0;
         }
-        return numDistinct(S, 0, T, 0);
-    }
-	
-	private int numDistinct(String S, int start1, String T, int start2)
-    {
-		if (start2>=T.length())
-		{
-			// the search has finished
-			return 1;
-		}
-        // main algorithm
-        int result=0;
-        // instead of trying every possible location, we in fact 
-        // only check the possible ones
-        int i=start1;
-        while(S.length()-i>=T.length()-start2)
+        if (S.charAt(i)==T.charAt(j))
         {
-            if (S.charAt(i)==T.charAt(start2))
-            {
-                // this is a valid starting location
-                // however we have two possibilties, use it or skip it
-                // use it
-                result+=numDistinct(S, i+1, T, start2+1);
-            }
-            i++;
+            return numDistinct(S, i+1, T, j+1)+numDistinct(S, i+1, T, j);
         }
-        return result;
+        else
+        {
+            return numDistinct(S, i+1, T, j);
+        }
     }
 }
