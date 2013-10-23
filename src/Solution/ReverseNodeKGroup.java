203d27729dfe8e413553ebb6c134ec8178c5cfd1
zhangtemplar <zhangtemplar@gmail.com>
2013-10-22 23:04:53 -0700

    Update ReverseNodeKGroup.java
    Submission Result: Compile Error
    
    Line 58: subReverseKGroup(ListNode,int,ListNode) in Solution cannot be applied to (ListNode,int)

--- src/Solution/ReverseNodeKGroup.java
+++ src/Solution/ReverseNodeKGroup.java
@@ -1,110 +1,74 @@
-package Solution;
-
-public class ReverseNodeKGroup {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public ListNode reverseKGroup(ListNode head, int k) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // one easy solution is use stack, however, the space is O(k)
-        // we can save the space by following procedure
-        // s: the previous of the first node of current group;
-        // p: another point
-        // set s=p; c=s;
-        // move s down the list by k times
-        // if met any null node, break
-        // for i=k-1:-1:0
-        //      set c=s;
-        //      move p down the list by i times
-        //      set n=s.next
-        //      set s.next=c.next;
-        //      set c.next=c.next.next;
-        //      set s=n;
-        // end
-        // this procedure process one group.
-        // move s down the list by k times
-        // is s is null, stop
-        // point
-        // base
-        if (k<2)
-        {
-            return head;
-        }
-        // create an auxillary node
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // one idea is to use recursion
+        // auxillary head
         ListNode result=new ListNode(0);
         result.next=head;
-        head=result;
-        while (true)
+        ListNode start=result;
+        while(start!=null)
         {
-            // scan the the list to ensure sufficient length
-            // the length is not enough
-            if (!isLong(head.next, k))
+            ListNode tail=start;
+            // find the start of next segment
+            int i=0;
+            while(i<=k && tail!=null)
+            {
+                tail=tail.next;
+                i++;
+            }
+            // the length of tail is not a multiple of k
+            if (tail==null && i<=k)
             {
                 break;
             }
-            // swap it 
-            head=swapKGroup(head, k);
-            // move the pointer
+            // reverse the current segment, which will return the last element of the reversed segment
+            start=subReverseKGroup(start.next, k, start);
+            if (start==null)
+            {
+                break;
+            }
+            // connect current segment to the next one
+            start.next=tail;
         }
         return result.next;
     }
     
-    /*
-    * this function checks whether the list is long enough
-    */
-    private boolean isLong(ListNode head, int k)
+    /**
+     * @param head the start of current segment
+     * @k the length of current segment
+     * @start the last element of previous segment
+     * @return the last element of reverse of current segment, i.e., the start of current segment before reverse
+     */ 
+    private ListNode subReverseKGroup(ListNode head, int k, ListNode start)
     {
-        ListNode current=head;
-        int i=0;
-        while (current!=null && i++<k)
+        // recursion
+        if (k>1 && head!=null)
         {
-            current=current.next;
+            ListNode node=subReverseKGroup(head.next, k-1, start);
+            if (node==null)
+            {
+                return null;
+            }
+            head.next=null;
+            node.next=head;
+            return head;
         }
-        // the length is not enough
-        if (i<k)
-        {
-            return false;
-        }
+        // base: we reach the tail
         else
         {
-            return true;
+            start.next=head;
+            return head;
         }
-    }
-    
-    /*
-    * this function swap within the k group
-    * head is the previous point of the start of the current group
-    */
-    private ListNode swapKGroup(ListNode head, int k)
-    {
-        ListNode start, current, previous, next;
-        int j;
-        start=head;
-        // we only need to swap k-1 times
-        for (int i=k-1; i>0; i--)
-        {
-            current=start;
-            j=i;
-            // move the pointer
-            while(j-->0)
-            {
-                current=current.next;
-            }
-            // apply the swapping
-            next=start.next;
-            start.next=current.next;
-            current.next=current.next.next;
-            start.next.next=next;
-            start=start.next;
-        }
-        // return the last element of current node
-        return start.next;
     }
 }
