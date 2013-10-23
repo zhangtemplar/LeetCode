c50f50920770aff8ab8d6f8711d24e098a49ad0c
zhangtemplar <zhangtemplar@gmail.com>
2013-10-22 23:49:47 -0700

    Update MergeKSortedList.java
    Submission Result: Compile Error
    
    Line 31: cannot find symbol: constructor ListNode(ListNode)
    
    Submission Result: Runtime Error: we need to provide a comparator for the listnode
    
    Last executed input:	[{1},{0}]

--- src/Solution/MergeKSortedList.java
+++ src/Solution/MergeKSortedList.java
@@ -1,110 +1,59 @@
-package Solution;
-
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-
-public class MergeKSortedList {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public ListNode mergeKLists(ArrayList<ListNode> lists) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // the idea is simple, we maintain a min heap, whose size is k
-        // for each iteration, we pop the top of the heap and add to the output
-        // accordingly we add the next element of the array, from which the poped element comes
-        // the complexity is O(sum(n_i)log(k)), where n_i is the length of i_th list
-        // when k is 2, the complexity is linear
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ArrayList<ListNode> lists) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // just like ordinary sort list, however, we will use a min heap to maintain the minimal element
         if (lists==null || lists.size()<1)
         {
-            // illegal input
             return null;
         }
-        else if (lists.size()==1)
+        // we use the priority queue to maintain the minimal element of all lists
+        // the size of the queue is equal to the size of current nonempty list
+        PriorityQueue<ListNode> queue=new PriorityQueue<ListNode>(lists.size(), new ListNodeComp());
+        // initialize the queue by adding the head of each list
+        for(ListNode node: lists)
         {
-            // no merge is required
-            return lists.get(0);
+            if (node!=null)
+            {
+                queue.add(node);
+            }
         }
-        else
+        ListNode result=new ListNode(0);
+        ListNode head=result;
+        while(!queue.isEmpty())
         {
-            // apply the sort
-            // maintain an array of pointer for each list
-            ListNode []pointer=new ListNode[lists.size()];
-            // the head, to-add node, latest node of the resultant list
-            ListNode result, previous;
-            NodeIndex current;
-            result=null;
-            current=null;
-            previous=null;
-            // initialize
-            PriorityQueue<NodeIndex> queue=new PriorityQueue<NodeIndex>(lists.size());
-            for (int i=0; i<lists.size(); i++)
+            // in each step, we always add the minimal of the queue, i.e., head
+            head.next=queue.poll();
+            head=head.next;
+            // then we add the next element from the list whose node we just add to the new list
+            if (head.next!=null)
             {
-                // for the pointer of each list
-                pointer[i]=lists.get(i);
-                // add it to the min heap
-                if (pointer[i]!=null)
-                {
-                    queue.add(new NodeIndex(pointer[i], i));
-                }
+                queue.add(head.next);
             }
-            // apply the merge
-            while (true)
-            {
-                // exit condition
-                if (queue.size()<1)
-                {
-                    break;
-                }
-                // pop the top\
-                current=queue.poll();
-                // add the head of the result
-                if (result==null)
-                {
-                    result=current.node;
-                }
-                // add the latest eleent to the resultant list
-                if (previous!=null)
-                {
-                    previous.next=current.node;
-                    previous=current.node;
-                }
-                else
-                {
-                    previous=current.node;
-                }
-                // add the next element of that list into the heap
-                pointer[current.index]=current.node.next;
-                if (pointer[current.index]!=null)
-                {
-                    queue.add(new NodeIndex(pointer[current.index], current.index));
-                }
-            }
-            return result;
         }
+        return result.next;
     }
 }
 
-class NodeIndex implements Comparable<NodeIndex>
+class ListNodeComp implements Comparator<ListNode>
 {
-    public ListNode node;
-    public int index;
-    public NodeIndex(ListNode n, int i)
+    public int compare(ListNode x, ListNode y)
     {
-        node=n;
-        index=i;
+        return x.val-y.val;
     }
     
-    // this is required for building the min heap
-    // the order only depends on the value of the node
-    public int compareTo(NodeIndex n)
+    public boolean equals(Object o)
     {
-        return node.val-n.node.val;
+        return false;
     }
 }
