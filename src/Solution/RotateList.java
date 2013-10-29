2b4c05ada0575a8fd3bac47c6ebc7b0c78495c82
zhangtemplar <zhangtemplar@gmail.com>
2013-10-28 23:47:11 -0700

    Update RotateList.java
    Submission Result: Compile Error
    
    Line 18: missing return value
    
    Submission Result: Wrong Answer
    
    Input:	{1,2}, 3
    Output:	{1,2}
    Expected:	{2,1}
    
    Submission Result: Compile Error
    
    Line 33: missing return value

--- src/Solution/RotateList.java
+++ src/Solution/RotateList.java
@@ -1,92 +1,60 @@
-package Solution;
-
-import java.util.LinkedList;
-
-public class RotateList {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		RotateList instance=new RotateList();
-		ListNode n1=new ListNode(1);
-		n1.next=new ListNode(2);
-		ListNode n2=instance.rotateRight(n1, 2);
-	}
-
-	public ListNode rotateRight(ListNode head, int n) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // scan the last element to rotate
-        // check the condition
-        if (n<1 || head==null)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode rotateRight(ListNode head, int n) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        if (head==null || n<1)
         {
             return head;
         }
-        // we use a queue to find the k-element on the right
-        ListNode current=head;
-        ListNode removed=null;
+        // create an auxillary head
+        ListNode result=new ListNode(0);
+        result.next=head;
+        // identify the length of the array
+        int len=0;
+        ListNode ptr=head;
+        while(ptr!=null)
+        {
+            len++;
+            ptr=ptr.next;
+        }
+        if (len==1)
+        {
+            return head;
+        }
+        n=n%len;
+        // find the n-th node
         LinkedList<ListNode> queue=new LinkedList<ListNode>();
-        while(current!=null)
+        ptr=head;
+        while(ptr!=null)
         {
-            if (queue.size()>=n)
+            if(queue.size()>n)
             {
-                removed=queue.remove();
+                queue.remove();
             }
-            queue.add(current);
-            current=current.next;
+            queue.add(ptr);
+            ptr=ptr.next;
         }
-        // check the condition
-        // whether the array is long enough
-        if (queue.size()<n || removed==null)
+        // check whether there is enough node there
+        if (queue.size()>n)
         {
-            // the queue is not long enough
-            // we leave it untouched
-            // return head;
-            // we need to make it cycle
-            n%=queue.size();
-            if (n==0)
-            {
-                return head;
-            }
-            current=head;
-            removed=null;
-            queue.clear();
-            while(current!=null)
-            {
-                if (queue.size()>=n)
-                {
-                    removed=queue.remove();
-                }
-                queue.add(current);
-                current=current.next;
-            }
+            // the first of the queue is the position we want to change
+            ptr=queue.getLast();
+            ptr.next=result.next;
+            ptr=queue.getFirst();
+            result.next=ptr.next;
+            ptr.next=null;
         }
-        // length>n
-        // set the head
-        ListNode result=queue.remove();
-        // find the tail
-        current=result;
-        while(current.next!=null)
-        {
-            current=current.next;
-        }
-        // concatenate
-        current.next=head;
-        // remove the cycle
-        removed.next=null;
-        return result;
+        return result.next;
     }
 }
-
-//class ListNode 
-//{
-//	int val;
-//	ListNode next;
-//	ListNode(int x) 
-//	{
-//	val = x;
-//	next = null;
-//	}
-//}
