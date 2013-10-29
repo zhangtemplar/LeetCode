ca3df755546bef8e4ce96152092b670de170a7aa
zhangtemplar <zhangtemplar@gmail.com>
2013-10-29 00:08:44 -0700

    Update FindMissingPositive.java
    Submission Result: Compile Error
    
    Line 20: cannot find symbol: variable target
    
    Submission Result: Wrong Answer L 17, 45
    
    Input:	[1]
    Output:	1
    Expected:	2

--- src/Solution/FindMissingPositive.java
+++ src/Solution/FindMissingPositive.java
@@ -1,80 +1,47 @@
-package Solution;
-
-public class FindMissingPositive {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public int firstMissingPositive(int[] A) 
-	{
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we try to store the result to the array itself
-        // we start from i=0;
-        // if A[i]>0, we store it to A[A[i]-1] if possible
-        // at the end, we just scan from the begining.
-        // the difficulty is how to store the number it original in
+public class Solution {
+    public int firstMissingPositive(int[] A) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        // obviously, this number should be smaller than A.length+1
+        // so we can store the found positive number in A
+        //  if A[i] is negative, we ignore it;
+        //  if A[i]>A.length, we also ignore it;
+        //  otherwise, we store it to A[A[i]-1]. Note, we may need to move the element already there
         if (A==null || A.length<1)
         {
             return 1;
         }
-        int swap=-1;
-        int i=0;
-        while(i<A.length)
+        for(int i=0; i<A.length; i++)
         {
-            // we need to process swap first
-            if (swap>0)
+            // we need to save this number
+            if (A[i]>0 && A[i]<=A.length)
             {
-                if (A[swap-1]<0 || A[swap-1]>A.length || A[swap-1]==swap)
+                // we need to shift some element out
+                int target=A[i];
+                while(A[target-1]>0 && A[target-1]<=A.length && A[target-1]!=target)
                 {
-                    // we are lucky, we just put it here
-                    A[swap-1]=swap;
-                    swap=-1;
+                    int tmp=A[target-1];
+                    A[target-1]=target;
+                    target=tmp;
                 }
-                else
+                if (target>0 && target<=A.length)
                 {
-                    // we need to repear this process
-                    int tmp=A[swap-1];
-                    A[swap-1]=swap;
-                    swap=tmp;
+                    A[target-1]=target;
                 }
             }
             else
             {
-                if (A[i]>0 && A[i]<=A.length && A[i]-1!=i)
-                {
-                    // we need to save this number
-                    if (A[A[i]-1]<0 || A[A[i]-1]>A.length || A[A[i]-1]==A[i])
-                    {
-                        // it is a negative number, we don't care and overwrite it
-                        // it this number is too large, we can safely ignore it also
-                        A[A[i]-1]=A[i];
-                        A[i]=-1;
-                    }
-                    else
-                    {
-                        // this is another positive number, we need to store it somewhere else
-                        swap=A[A[i]-1];
-                        A[A[i]-1]=A[i];
-                        A[i]=-1;
-                        // we need to find a proper position to store swap
-                    }
-                }
-                i++;
+                A[i]=0;
             }
         }
-        for (i=0; i<A.length; i++)
+        // just scan from 0 to the end
+        for(int i=0; i<A.length; i++)
         {
             if (A[i]!=i+1)
             {
                 return i+1;
             }
         }
-        return i+1;
+        return A.length+1;
     }
 }
