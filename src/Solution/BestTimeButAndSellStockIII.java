5bac4183e3612d0acaf42aae7d898148ae56bbb6
zhangtemplar <zhangtemplar@gmail.com>
2013-10-28 23:20:23 -0700

    Update BestTimeButAndSellStockIII.java
    Submission Result: Runtime Error
    
    Last executed input:	[]

--- src/Solution/BestTimeButAndSellStockIII.java
+++ src/Solution/BestTimeButAndSellStockIII.java
@@ -1,138 +1,80 @@
-package Solution;
-
-import java.util.Arrays;
-
-public class BestTimeButAndSellStockIII {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		BestTimeButAndSellStockIII instance=new BestTimeButAndSellStockIII();
-		int[] prices={6,1,3,2,4,7};
-		System.out.println(instance.maxProfit(prices));
-	}
-	
-	public int maxProfit(int[] prices)
-	{
-		// a more efficient way is that, we process the array from two direction
-		// and merge the result
-		// O(N)
-		if (prices==null || prices.length<2)
-        {
-            return 0;
-        }
-		int []forward=maxProfitForward(prices);
-		int []backward=maxProfitBackward(prices);
-		int profit=forward[prices.length-1];
-		// we try different cutting location for two transaction
-		for (int i=0; i<prices.length-1; i++)
-		{
-			// transaction happesn between [0, i] and [i+1, n-1]
-			int tmp=forward[i]+backward[i+1];
-            if (tmp>profit)
-            {
-                profit=tmp;
-            }
-		}
-		return profit;
-	}
-	
-	// we start the array from the end of the array
-	// note, we change the logic slightly
-	// we record the highest (sell) price until the current location
-	private int[] maxProfitBackward(int[] prices)
-	{
-		int i=prices.length-2;
-        int j=prices.length-1;
-        // profit track the maximal profit available
-        // when the buy location is no earlier than i
-        if (prices==null || prices.length<1)
-        {
-            return null;
-        }
-        int []profit=new int[prices.length];
-        profit[prices.length-1]=0;
-        if (i<0)
-        {
-        	return profit;
-        }
-        for (; i>=0; i--)
-        {
-            if (prices[j]<prices[i+1])
-            {
-            	// ok, we have a higher sell price
-                j=i+1;
-            }
-            if (profit[i+1]<prices[j]-prices[i])
-            {
-            	profit[i]=prices[j]-prices[i];
-            }
-            else
-            {
-            	profit[i]=profit[i+1];
-            }
-        }
-        return profit;
-	}
-	
-	// it tracks the profit where the sell position is no later than i
-	private int[] maxProfitForward(int []prices)
-	{
-		int i=0;
-        int j=1;
-        int max=Integer.MIN_VALUE;
-        if (prices==null || prices.length<1)
-        {
-            return null;
-        }
-        int []profit=new int[prices.length];
-        profit[0]=0;
-        if (j>=prices.length)
-        {
-        	return profit;
-        }
-        for (; j<prices.length; j++)
-        {
-            if (prices[i]>prices[j-1])
-            {
-                i=j-1;
-            }
-            if (profit[j-1]<prices[j]-prices[i])
-            {
-            	profit[j]=prices[j]-prices[i];
-            }
-            else
-            {
-            	profit[j]=profit[j-1];
-            }
-        }
-        return profit;
-	}
-
-	public int maxProfitSlow(int[] prices) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // a simple algorithm is the following
-        // we cut the array into halves and apply the solution of orignal algorithm
-        // on each half and record their sum
-        // we try all possible location to find the maximum
-		// complexity is O(N^2)
+public class Solution {
+    public int maxProfit(int[] prices) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
         if (prices==null || prices.length<2)
         {
             return 0;
         }
-        BestTimeButAndSellStock instance=new BestTimeButAndSellStock();
-        int profit=instance.maxProfit(prices);
-        for (int i=2; i<prices.length; i++)
+        // we will use the first version
+        // in addition, we will search for the partition (from 0:n), such that each parition includes only one operation
+        int []left=leftProfit(prices);
+        int []right=rightProfit(prices);
+        // find the optimal partition: at least better than one transaction case
+        int max=right[0];
+        for (int i=1; i<prices.length; i++)
         {
-            int tmp=instance.maxProfit(Arrays.copyOfRange(prices, 0, i))+instance.maxProfit(Arrays.copyOfRange(prices, i, prices.length));
-            if (tmp>profit)
+            // may not engage in multiple transactions at the same time
+            // [0:i-1][i:n-1]
+            int profit=left[i-1]+right[i];
+            if (profit>max)
             {
-                profit=tmp;
+                max=profit;
             }
         }
-        return profit;
+        return max;
+    }
+    
+    // result[i] is the max profit we can get if all the operation happens before i
+    private int[] leftProfit(int []prices)
+    {
+        int []result=new int[prices.length];
+        int buy=prices[0];
+        for (int i=1; i<prices.length; i++)
+        {
+            // update the buy price, if we find a lower one
+            if (buy>prices[i-1])
+            {
+                buy=prices[i-1];
+            }
+            // check the profit
+            int profit=prices[i]-buy;
+            // current profit is better
+            if (profit>result[i-1])
+            {
+                result[i]=profit;
+            }
+            // previous profit is better
+            else
+            {
+                result[i]=result[i-1];
+            }
+        }
+        return result;
+    }
+    
+    // similar as the previous one, however we require all operation after step i
+    private int[] rightProfit(int []prices)
+    {
+        int []result=new int[prices.length];
+        int sell=prices[prices.length-1];
+        for (int i=prices.length-2; i>=0; i--)
+        {
+            // a higher sell price
+            if (sell<prices[i+1])
+            {
+                sell=prices[i+1];
+            }
+            int profit=sell-prices[i];
+            if (profit>result[i+1])
+            {
+                result[i]=profit;
+            }
+            else
+            {
+                result[i]=result[i+1];
+            }
+        }
+        return result;
     }
 }
