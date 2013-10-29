a8ef39fb0c37de5715cffee8482e55c5cbb34dda
zhangtemplar <zhangtemplar@gmail.com>
2013-10-28 23:49:16 -0700

    Update RecoverBinarySearchTree.java

--- src/Solution/RecoverBinarySearchTree.java
+++ src/Solution/RecoverBinarySearchTree.java
@@ -1,95 +1,71 @@
-package Solution;
-
-import java.util.HashSet;
-import java.util.Stack;
-
-public class RecoverBinarySearchTree {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TreeNode root=new TreeNode(2);
-		root.left=new TreeNode(3);
-		root.right=new TreeNode(1);
-		System.out.println(root);
-		RecoverBinarySearchTree instance=new RecoverBinarySearchTree();
-		instance.recoverTree(root);
-		System.out.println(root);
-	}
-
-	public void recoverTree(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we will use the same idea as "Validate Binary Search Tree"
-        // however, when we met the first violation, we record the node
-        // when we met the second violation, we swap this value with the value of recorded node
-        Stack<TreeNode> stack=new Stack<TreeNode>();
-        HashSet<TreeNode> visited=new HashSet<TreeNode>();
-        // note that, when we met a violation, in fact, we wont't know 
-        // which one is the violation, is the current one or previous one
-        // so make it simple, we store this two
-        TreeNode p_node=null;
-        TreeNode c_node=null;
-        TreeNode prev=null;
-        TreeNode node=null;
-        int c_value=Integer.MIN_VALUE;
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void recoverTree(TreeNode root) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        // by performing a in-order traverse, we can identify the node whose order is wrong
         if (root==null)
         {
             return;
         }
+        Stack<TreeNode> stack=new Stack<TreeNode>();
         stack.push(root);
+        HashSet<TreeNode> visit=new HashSet<TreeNode>();
+        int min=Integer.MIN_VALUE;
+        TreeNode left=null;
+        TreeNode right=null;
+        TreeNode prev=null;
         while(!stack.isEmpty())
         {
-            node=stack.pop();
-            if (visited.contains(node))
+            TreeNode node=stack.pop();
+            if (visit.contains(node))
             {
-                // we have visited this node (to process its children)
-                // we compare it to the value before it in in-order
-                // it is no smaller than the previous value
-                // we are good
-                if (c_value<node.val)
+                // there must be something wrong with this node
+                if (min>node.val)
                 {
-                    c_value=node.val;
+                    // we have found an error before
+                    if (left!=null)
+                    {
+                        int val=left.val;
+                        left.val=node.val;
+                        node.val=val;
+                        return;
+                    }
+                    // we need to save this node, in case the two nodes are adjacent
+                    else
+                    {
+                        left=prev;
+                        right=node;
+                    }
                 }
-                // otherwise, there must be something wrong.
-                else if(c_node==null)
-                {
-                    // record it
-                    c_node=node;
-                    p_node=prev;
-                    // recover the c_value
-                    c_value=node.val;
-                }
-                else
-                {
-                    // we need to identify the pair of swapped node
-                    // this is always the p_node and node
-                    int swap=p_node.val;
-                    p_node.val=node.val;
-                    node.val=swap;
-                    return;
-                }
+                min=node.val;
                 prev=node;
             }
             else
             {
+                visit.add(node);
                 if (node.right!=null)
                 {
                     stack.push(node.right);
                 }
-                visited.add(node);
                 stack.push(node);
-                if (node.left!=null)
+                if(node.left!=null)
                 {
                     stack.push(node.left);
                 }
             }
         }
-        // there could be a special condition, where the violation happens to be adjacent
-        int swap=p_node.val;
-        p_node.val=c_node.val;
-        c_node.val=swap;
+        // in case the swaped node are adjacent
+        int val=left.val;
+        left.val=right.val;
+        right.val=val;
     }
 }
