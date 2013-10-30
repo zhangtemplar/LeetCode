440b21018e33c0d7c121f479644d7b68dd70df8d
zhangtemplar <zhangtemplar@gmail.com>
2013-10-29 23:45:38 -0700

    Update MergeIntervals.java
    Submission Result: Compile Error
    
    Line 37: invalid method declaration; return type required
    
    Submission Result: Runtime Error
    
    Last executed input:	[]
    
    Submission Result: Wrong Answer
    
    Input:	[[1,3]]
    Output:	[]
    Expected:	[[1,3]]

--- src/Solution/MergeIntervals.java
+++ src/Solution/MergeIntervals.java
@@ -1,85 +1,68 @@
-package Solution;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-
-public class MergeIntervals {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MergeIntervals instance=new MergeIntervals();
-		ArrayList<Interval> data=new ArrayList<Interval>();
-		data.add(new Interval(1, 4));
-		data.add(new Interval(1, 4));
-		System.out.println(instance.merge(data));
-	}
-
-	public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if (intervals==null || intervals.size()<2)
-        {
-            return intervals;
-        }
-        Collections.sort(intervals, new Comp());
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
         ArrayList<Interval> result=new ArrayList<Interval>();
-        Interval current=intervals.get(0);
-        for (int i=1; i<intervals.size(); i++)
+        if (intervals==null || intervals.size()<1)
         {
-            if (isOverlap(current, intervals.get(i)))
+            return result;
+        }
+        // we first sort the intervals
+        Collections.sort(intervals, new IntervalComp());
+        // we combine the list one by one
+        Interval x=new Interval(intervals.get(0).start, intervals.get(0).end);
+        for (Interval y: intervals)
+        {
+            // find an overlap, merge it
+            if (isOverlap(x, y))
             {
-                // it is overlap, we update it
-                current.end=current.end>intervals.get(i).end?current.end:intervals.get(i).end;
+                x.start=Math.min(x.start, y.start);
+                x.end=Math.max(x.end, y.end);
             }
+            // otherwise create a new entry
             else
             {
-                // add the previous overlap it
-                result.add(current);
-                current=intervals.get(i);
+                result.add(x);
+                x=new Interval(y.start, y.end);
             }
         }
-        // add the final one in
-        result.add(current);
+        result.add(x);
         return result;
     }
     
-    // we assume x.start<=y.start
     private boolean isOverlap(Interval x, Interval y)
     {
-        if (y.start<=x.end)
+        if (x.start<y.start)
         {
-            return true;
+             return x.end>=y.start;
         }
-        return false;
-    }
-    
-    private static class Comp implements Comparator<Interval>
-    {
-        public int compare(Interval x, Interval y)
+        else
         {
-            return x.start-y.start;
-        }
-        
-        public boolean equals(Object obj)
-        {
-            return true;
+            return  y.end>=x.start;
         }
     }
 }
 
-//Definition for an interval.
-class Interval {
-    int start;
-    int end;
-    Interval() { start = 0; end = 0; }
-    Interval(int s, int e) { start = s; end = e; }
-    public String toString()
+class IntervalComp implements Comparator<Interval>
+{
+    public int compare(Interval x, Interval y)
     {
-    	return new String("["+start+","+end+"]");
+        if (x.start!=y.start)
+        {
+            return x.start-y.start;
+        }
+        else
+        {
+            return x.end-y.end;
+        }
     }
 }
