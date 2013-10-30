f4b6019798b1fbd8f57d0beb7ff7f1db6b62cccc
zhangtemplar <zhangtemplar@gmail.com>
2013-10-29 22:56:45 -0700

    Update RestoreIPAddresses.java
    Submission Result: Compile Error
    
    Line 6: cannot find symbol: variable length
    
    Submission Result: Compile Error
    
    Line 24: cannot find symbol: constructor String(int)
    
    Input:	"010010"
    Output:	["0.1.0.10","0.1.0.10","0.1.1.0","0.10.0.10","0.10.1.0","0.100.1.0","1.0.0.10","1.0.1.0","1.0.1.0","10.0.1.0"]
    Expected:	["0.10.0.10","0.100.1.0"]
    
    Submission Result: Compile Error
    
    Line 62: illegal start of expression
    
    Submission Result: Wrong Answer L 44
    
    Input:	"25525511135"
    Output:	[]
    Expected:	["255.255.11.135","255.255.111.35"]

--- src/Solution/RestoreIPAddresses.java
+++ src/Solution/RestoreIPAddresses.java
@@ -1,126 +1,84 @@
-package Solution;
-
-import java.util.ArrayList;
-
-public class RestoreIPAddresses {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		RestoreIPAddresses instance=new RestoreIPAddresses();
-		System.out.println(instance.restoreIpAddresses("010010"));
-	}
-
-	public ArrayList<String> restoreIpAddresses(String s) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we will use recursion, however, parse the string in triple
-        ArrayList<String> result=new ArrayList<String>();
-        // obviously illegal case
-        if (s==null || s.length()<4 || s.length()>12)
+public class Solution {
+    public ArrayList<String> restoreIpAddresses(String s) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        // we will use backtrace
+        if (s==null || s.length()<4)
         {
-            return result;
+            return new ArrayList<String>();
         }
-        int[] cuts=new int[3];
-        cuts[0]=1;
-        result.addAll(restoreIpAddresses(s, cuts, 1));
-        cuts[0]=2;
-        result.addAll(restoreIpAddresses(s, cuts, 1));
-        cuts[0]=3;
-        result.addAll(restoreIpAddresses(s, cuts, 1));
-        return result;
+        return restoreIpAddresses(s, 0, new ArrayList<Integer>());
     }
     
-    // start indicates the string we are parsing
-    // cuts for the current cutting position (the start of each segment)
-    // n indicates the number of segements already obtained
-    private ArrayList<String> restoreIpAddresses(String s, int[] cuts, int n)
+    /**
+     * @param start is the index of char we can use
+     */ 
+    private ArrayList<String> restoreIpAddresses(String s, int start, ArrayList<Integer> base)
     {
         ArrayList<String> result=new ArrayList<String>();
-        // we first check the string are good so far
-        if (cuts[n-1]>=s.length())
+        // we finish the string
+        if (start>=s.length())
         {
-            return result;
-        }
-        // first we will check the previous section is valid or not
-        long val;
-        if (n==1)
-        {
-            if (!validSegment(s, 0, cuts[0]))
+            if (base.size()==4)
             {
-                return result;
+                StringBuffer str=new StringBuffer();
+                str.append(base.get(0));
+                str.append('.');
+                str.append(base.get(1));
+                str.append('.');
+                str.append(base.get(2));
+                str.append('.');
+                str.append(base.get(3));
+                result.add(str.toString());
             }
-            val=Long.parseLong(s.substring(0, cuts[0]));
         }
-        else
+        // the string is not finished yet and the ip address is not complete yet
+        // however, we need to avoid '00' of something similar
+        else if(base.size()<4)
         {
-            if (!validSegment(s, cuts[n-2], cuts[n-1]))
+            ArrayList<Integer> entry=null;
+            // we can have triple here
+            // 1    the string is not long enough
+            // 2    the string has value >255
+            // 3    the string has something like 0xx
+            if (start+2<s.length() && (s.charAt(start)-'0')*100+(s.charAt(start+1)-'0')*10+(s.charAt(start+2)-'0')<=255 && (s.charAt(start)-'0')*100+(s.charAt(start+1)-'0')*10+(s.charAt(start+2)-'0')>99)
             {
-                return result;
+                // add this single char
+                entry=new ArrayList<Integer>(base);
+                entry.add(s.charAt(start)-'0');
+                result.addAll(restoreIpAddresses(s, start+1, entry));
+                // also check the two char
+                entry=new ArrayList<Integer>(base);
+                entry.add((s.charAt(start)-'0')*10+(s.charAt(start+1)-'0'));
+                result.addAll(restoreIpAddresses(s, start+2, entry));
+                // and triple
+                entry=new ArrayList<Integer>(base);
+                entry.add((s.charAt(start)-'0')*100+(s.charAt(start+1)-'0')*10+(s.charAt(start+2)-'0'));
+                result.addAll(restoreIpAddresses(s, start+3, entry));
             }
-        	val=Long.parseLong(s.substring(cuts[n-2], cuts[n-1]));
-        }
-        // this is not a valid segment
-        if (val>255 || val<0)
-        {
-            return result;
-        }
-        // everything is good so far
-        if (n==3)
-        {
-            // we have finished the cutting, but need to check the last segment
-        	if (!validSegment(s, cuts[n-1], s.length()))
+            // we can have double
+            // 1    the string is not long enough
+            // 2    the string is something like 0x
+            else if(start+1<s.length() && (s.charAt(start)-'0')*10+(s.charAt(start+1)-'0')>9)
             {
-                return result;
+                // add this single char
+                entry=new ArrayList<Integer>(base);
+                entry.add(s.charAt(start)-'0');
+                result.addAll(restoreIpAddresses(s, start+1, entry));
+                // also check the two char
+                entry=new ArrayList<Integer>(base);
+                entry.add((s.charAt(start)-'0')*10+(s.charAt(start+1)-'0'));
+                result.addAll(restoreIpAddresses(s, start+2, entry));
             }
-            val=Integer.parseInt(s.substring(cuts[n-1], s.length()));
-            if (val>255 || val<0)
-            {
-                return result;
-            }
+            // only single
             else
             {
-                result.add(string2IP(s, cuts));
-                return result;
+                // add this single char
+                entry=new ArrayList<Integer>(base);
+                entry.add(s.charAt(start)-'0');
+                result.addAll(restoreIpAddresses(s, start+1, entry));
             }
         }
-        else
-        {
-            // just found the possible cutting location
-            for (int i=cuts[n-1]+1; i<s.length(); i++)
-            {
-                cuts[n]=i;
-                result.addAll(restoreIpAddresses(s, cuts, n+1));
-                cuts[n]=0;
-            }
-            return result;
-        }
-    }
-    
-    private boolean validSegment(String s, int start, int end)
-    {
-    	if (start==end-1)
-    	{
-    		return true;
-    	}
-    	else
-    	{
-    		return s.charAt(start)!='0';
-    	}
-    }
-    
-    private String string2IP(String s, int[]cuts)
-    {
-        StringBuffer result=new StringBuffer();
-        result.append(s.substring(0, cuts[0]));
-        result.append('.');
-        result.append(s.substring(cuts[0], cuts[1]));
-        result.append('.');
-        result.append(s.substring(cuts[1], cuts[2]));
-        result.append('.');
-        result.append(s.substring(cuts[2], s.length()));
-        return result.toString();
+        return result;
     }
 }
