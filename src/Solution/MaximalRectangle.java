478b71f459fb05f36efcce37c016739f2b5fd52b
zhangtemplar <zhangtemplar@gmail.com>
2013-10-31 00:45:33 -0700

    Update MaximalRectangle.java

--- src/Solution/MaximalRectangle.java
+++ src/Solution/MaximalRectangle.java
@@ -1,66 +1,143 @@
-package Solution;
-
-public class MaximalRectangle {
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		char [][]matrix={{'0', '0', '0', '1', '0', '1', '1', '1'},
-			{'0', '1', '1', '0', '0', '1', '0', '1'},
-			{'1', '0', '1', '1', '1', '1', '0', '1'},
-			{'0', '0', '0', '1', '0', '0', '0', '0'},
-			{'0', '0', '1', '0', '0', '0', '1', '0'},
-			{'1', '1', '1', '0', '0', '1', '1', '1'},
-			{'1', '0', '0', '1', '1', '0', '0', '1'},
-			{'0', '1', '0', '0', '1', '1', '0', '0'},
-			{'1', '0', '0', '1', '0', '0', '0', '0'}};
-		MaximalRectangle instance=new MaximalRectangle();
-		System.out.println(instance.maximalRectangle(matrix));
-	}
-
-	public int maximalRectangle(char[][] matrix) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we need to use the algorithm from the "largest rectangle in histogram" to sovle
-        // this problem
+public class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        // we will use the algorithm of maximal rectange in histogram
+        // where the histogram is the # of 1s in this column starting from current row
         if (matrix==null || matrix.length<1 || matrix[0]==null || matrix[0].length<1)
         {
             return 0;
         }
-        // we will create a table, which stores the number of continus row for the row
-        // starting at this position
-        int [][]table=new int[matrix.length][matrix[0].length];
+        int[][]height=new int[matrix.length][matrix[0].length];
+        int max=0;
         for (int i=matrix.length-1; i>=0; i--)
         {
-            for (int j=0; j<matrix[0].length; j++)
+            for(int j=0; j<matrix[i].length; j++)
             {
                 if (matrix[i][j]=='0')
                 {
-                    table[i][j]=0;
+                    height[i][j]=0;
                 }
                 else if (i==matrix.length-1)
                 {
-                    table[i][j]=1;
+                    height[i][j]=1;
                 }
                 else
                 {
-                    table[i][j]=table[i+1][j]+1;
+                    height[i][j]=height[i+1][j]+1;
                 }
             }
-        }
-        // then for each row, we will apply algorithm "largest rectangle in histogram"
-        // and save which ever is the largest
-        int max_area=0;
-        LargestRectangleInHistogram instance=new LargestRectangleInHistogram();
-        for (int i=0; i<matrix.length; i++)
-        {
-            int area=instance.largestRectangleArea(table[i]);
-            if (area>max_area)
+            int area=largestRectangleArea(height[i]);
+            if (area>max)
             {
-                max_area=area;
+                max=area;
             }
         }
-        return max_area;
+        return max;
+        
+    }
+    
+    public int largestRectangleArea(int[] height) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        // we will have o(n) solution
+        // assume we have two array left and right, where
+        // left[i]=max{j: height[j]<height[i] & j<=i}
+        // right[i]=min{j: height[j]<height[i] & j>=i}
+        // then we need to look for max{i: (right[i]-left[i]-1)*height[i]}
+        if (height==null || height.length<1)
+        {
+            return 0;
+        }
+        int []left=findLeft(height);
+        int []right=findRight(height);
+        int max=0;
+        for (int i=0; i<height.length; i++)
+        {
+            int width=right[i]-left[i]-1;
+            if (height[i]<=height[right[i]])
+            {
+                width++;
+            }
+            if (height[i]<=height[left[i]])
+            {
+                width++;
+            }
+            int area=width*height[i];
+            if (area>max)
+            {
+                max=area;
+            }
+        }
+        return max;
+    }
+    
+    // left[i]=max{j: height[j]<height[i] & j<=i}
+    // the largest index on the left whose value is smaller than the current one
+    private int[] findLeft(int []height)
+    {
+        int []left=new int[height.length];
+        // stack stores the index of previous minimal element
+        Stack<Integer> stack=new Stack<Integer>();
+        stack.push(0);
+        for (int i=1; i<height.length; i++)
+        {
+            // simplest case
+            if (height[i]>height[i-1])
+            {
+                left[i]=i-1;
+            }
+            else
+            {
+                while(!stack.isEmpty() && height[i]<=height[stack.peek()])
+                {
+                    stack.pop();
+                }
+                // height[i] is the smallest element in the stack
+                if (stack.isEmpty())
+                {
+                    left[i]=0;
+                }
+                else
+                {
+                    left[i]=stack.peek();
+                }
+            }
+            stack.push(i);
+        }
+        return left;
+    }
+    
+    private int[] findRight(int []height)
+    {
+        int []right=new int[height.length];
+        // stack stores the index of previous minimal element
+        Stack<Integer> stack=new Stack<Integer>();
+        stack.push(height.length-1);
+        right[height.length-1]=height.length-1;
+        for (int i=height.length-2; i>=0; i--)
+        {
+            if (height[i]>height[i+1])
+            {
+                right[i]=i+1;
+            }
+            else
+            {
+                while(!stack.isEmpty() && height[i]<=height[stack.peek()])
+                {
+                    stack.pop();
+                }
+                if (stack.isEmpty())
+                {
+                    right[i]=height.length-1;
+                }
+                else
+                {
+                    right[i]=stack.peek();
+                }
+            }
+            stack.push(i);
+        }
+        return right;
     }
 }
