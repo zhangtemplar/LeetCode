a37a93078c3b56f0c6574ed29a249fcc8ce85839
zhangtemplar <zhangtemplar@gmail.com>
2013-10-30 23:34:11 -0700

    Update SudokuSolver.java
    Submission Result: Wrong Answer L 91
    
    Input:	["..9748...","7........",".2.1.9...","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."]
    Output:	["139748652","745326189","826159437","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."]
    Expected:	["519748632","783652419","426139875","357986241","264317598","198524367","975863124","832491756","641275983"]

--- src/Solution/SudokuSolver.java
+++ src/Solution/SudokuSolver.java
@@ -1,77 +1,108 @@
 public class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        // we just check whether each row/column/cell contain duplicate elements
-        if (board==null || board.length!=9 || board[0]==null || board[0].length!=9)
+    public void solveSudoku(char[][] board) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        // the basic idea is simple, we check each cell, row and column to put the solution in
+        // we will use backtrace
+        solveSudoku(board, 0, 0);
+    }
+    
+    /**
+     * this function try a value at position (row, col), if the value there is unkown
+     * if this value is valid so far, we will continue to the next cell
+     * otherwise, we take one step back
+     * @param board the current board
+     * @param row col the position we are currently working on
+     * @return whether current configuration is valid or not
+     */
+    private boolean solveSudoku(char[][] board, int row, int col)
+    {
+        // we alread finish the board
+        if (row>=board.length || col>=board[row].length)
         {
+            return true;
+        }
+        // this position is known
+        else if (board[row][col]!='.')
+        {
+            // we just check the next ones
+            if (col==board[row].length-1)
+            {
+                return solveSudoku(board, row+1, 0);
+            }
+            else
+            {
+                return solveSudoku(board, row, col+1);
+            }
+        }
+        // try value in this position
+        else
+        {
+            // we check every value from '1' to '9'
+            for (char i='1'; i<='9'; i++)
+            {
+                boolean used=false;
+                // check whether i is used in the row
+                for (int j=0; j<board[row].length; j++)
+                {
+                    if (board[row][j]==i)
+                    {
+                        used=true;
+                        break;
+                    }
+                }
+                // this value is used, so try the next value
+                if (used)
+                {
+                    continue;
+                }
+                // then check the column
+                for (int j=0; j<board.length; j++)
+                {
+                    if (board[j][col]==i)
+                    {
+                        used=true;
+                        break;
+                    }
+                }
+                if (used)
+                {
+                    continue;
+                }
+                // check the 3x3 cell
+                for (int j=row/3*3; j<(row/3+1)*3; j++)
+                {
+                    for (int l=col/3*3; l<(col/3+1)*3; l++)
+                    {
+                        if (board[j][l]==i)
+                        {
+                            used=true;
+                            break;
+                        }
+                    }
+                }
+                if (used)
+                {
+                    continue;
+                }
+                // so far so good, so we can continue with this value
+                board[row][col]=i;
+                // check the remaining
+                if (col==board[row].length-1)
+                {
+                    if (solveSudoku(board, row+1, 0))
+                    {
+                        return true;
+                    }
+                }
+                else if (solveSudoku(board, row, col+1))
+                {
+                    return true;
+                }
+                // this value dosen't work, so we take one step back and recover the value
+                board[row][col]='.';
+            }
             return false;
         }
-        // for each row
-        for (int i=0; i<9; i++)
-        {
-            HashSet<Character> set=new HashSet<Character>();
-            for (int j=0; j<9; j++)
-            {
-                if (board[i][j]>='0' && board[i][j]<='9')
-                {
-                    // a duplicate detected
-                    if (set.add(board[i][j])==false)
-                    {
-                        return false;
-                    }
-                }
-                else if(board[i][j]!='.')
-                {
-                    return false;
-                }
-            }
-        }
-        // for each row
-        for (int i=0; i<9; i++)
-        {
-            HashSet<Character> set=new HashSet<Character>();
-            for (int j=0; j<9; j++)
-            {
-                if (board[j][i]>='0' && board[j][i]<='9')
-                {
-                    // a duplicate detected
-                    if (set.add(board[j][i])==false)
-                    {
-                        return false;
-                    }
-                }
-                else if(board[j][i]!='.')
-                {
-                    return false;
-                }
-            }
-        }
-        // for each cell
-        for (int i=0; i<3; i++)
-        {
-            for (int j=0; j<3; j++)
-            {
-                HashSet<Character> set=new HashSet<Character>();
-                for (int ii=0; ii<3; ii++)
-                {
-                    for (int jj=0; jj<3; jj++)
-                    {
-                        if (board[i*3+ii][j*3+jj]>='0' && board[i*3+ii][j*3+jj]<='9')
-                        {
-                            // a duplicate detected
-                            if (set.add(board[i*3+ii][j*3+jj])==false)
-                            {
-                                return false;
-                            }
-                        }
-                        else if(board[i*3+ii][j*3+jj]!='.')
-                        {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-        return true;
     }
 }
