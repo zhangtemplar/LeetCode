9aee0ea0bb2e0bfa4aed70ef806367705bca77b2
zhangtemplar <zhangtemplar@gmail.com>
2013-10-31 21:54:24 -0700

    Update WordSearch.java
    Submission Result: Wrong Answer
    
    Input:	["a"], "a"
    Output:	false
    Expected:	true

--- src/Solution/WordSearch.java
+++ src/Solution/WordSearch.java
@@ -1,22 +1,11 @@
-package Solution;
-
-public class WordSearch {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		WordSearch instance=new WordSearch();
-		char[][]board={{'a'}};
-		String word="a";
-		System.out.println(instance.exist(board, word));
-	}
-
-	public boolean exist(char[][] board, String word) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // one way is too use DFS
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        if (board==null || board.length<1 || board[0]==null || board[0].length<1)
+        {
+            return word==null || word.length()<1;
+        }
         for (int i=0; i<board.length; i++)
         {
             for (int j=0; j<board[i].length; j++)
@@ -30,67 +19,50 @@ public class WordSearch {
         return false;
     }
     
-    /**
-     * check the substring for word[idx: end] with char at board[row][col]
-     */
-    private boolean exist(char [][]board, int row, int col, String word, int idx)
+    private boolean exist(char[][] board, int row, int col, String word, int start)
     {
-        if (idx>=word.length())
+        if (start>=word.length())
         {
             return true;
         }
+        // this word is used before
+        else if (board[row][col]==0)
+        {
+            return false;
+        }
         else
         {
-            if (word.charAt(idx)!=board[row][col])
+            // check current location
+            if (board[row][col]!=word.charAt(start))
             {
                 return false;
             }
-            else
+            else if(start==word.length()-1)
             {
-            	if (idx==word.length()-1)
-            	{
-            		return true;
-            	}
-                boolean result=false;
-                // mark out the curent word
-                board[row][col]=0;
-                if (row>0)
-                {
-                    result=exist(board, row-1, col, word, idx+1);
-                    if (result)
-                    {
-                        return true;
-                    }
-                }
-                if (row<board.length-1)
-                {
-                    result=exist(board, row+1, col, word, idx+1);
-                    if (result)
-                    {
-                        return true;
-                    }
-                }
-                if (col>0)
-                {
-                    result=exist(board, row, col-1, word, idx+1);
-                    if (result)
-                    {
-                        return true;
-                    }
-                }
-                if (col<board[row].length-1)
-                {
-                    result=exist(board, row, col+1, word, idx+1);
-                    if (result)
-                    {
-                        return true;
-                    }
-                }
-                // recover it
-                board[row][col]=word.charAt(idx);
-                return false;
+                return true;
             }
-            
+            // mark the current location
+            board[row][col]=0;
+            // check the next ones
+            if (row>0 && exist(board, row-1, col, word, start+1))
+            {
+                return true;
+            }
+            if (row<board.length-1 && exist(board, row+1, col, word, start+1))
+            {
+                return true;
+            }
+            if (col>0 && exist(board, row, col-1, word, start+1))
+            {
+                return true;
+            }
+            if (col<board[row].length-1 && exist(board, row, col+1, word, start+1))
+            {
+                return true;
+            }
+            // recover the current location
+            board[row][col]=word.charAt(start);
+            return false;
         }
     }
 }
