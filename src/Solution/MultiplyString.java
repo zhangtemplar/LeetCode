a972e30628af6b6b9242af0a954c391cc45fc4b1
zhangtemplar <zhangtemplar@gmail.com>
2013-10-31 23:20:53 -0700

    Update MultiplyString.java
    Submission Result: Compile Error
    
    Line 34: cannot find symbol: variable num12
    
    Runtime error for "0", "0"
    
    Submission Result: Wrong Answer
    
    Input:	"9", "9"
    Output:	"8"
    Expected:	"81"
    
    
    Submission Result: Wrong Answer
    
    Input:	"123", "456"
    Output:	"15048"
    Expected:	"56088"

--- src/Solution/MultiplyString.java
+++ src/Solution/MultiplyString.java
@@ -1,87 +1,89 @@
-package Solution;
-
-import java.util.ArrayList;
-
-public class MultiplyString {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MultiplyString instance=new MultiplyString();
-		System.out.println(instance.multiply("0", "0"));
-	}
-
-	public String multiply(String num1, String num2) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we first convert the input to an array list of integer
+public class Solution {
+    public String multiply(String num1, String num2) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
         if (num1==null || num1.length()<1 || num2==null || num2.length()<1)
         {
-            return new String();
+            return new String("0");
         }
-        // the most significant bit is put at the head
-        ArrayList<Integer> z=new ArrayList<Integer>();
-        // we start from the most siginificant digit
-        for (int i=0; i<num1.length(); i++)
+        // make sure num1 is shorter than num2
+        else if(num1.length()>num2.length())
         {
-            // shift the digit up by 1 digits
-            z.add(0);
-            for (int j=0; j<num2.length(); j++)
+            return multiply(num2, num1); 
+        }
+        // apply the multiplication
+        ArrayList<Integer> result=new ArrayList<Integer>();
+        for (int i=num1.length()-1; i>=0; i--)
+        {
+            // make the multiplication
+            int increment=0;
+            int sum=0;
+            ArrayList<Integer> buffer=new ArrayList<Integer>();
+            for (int j=num2.length()-1; j>=0; j--)
             {
-                if(z.size()<=i+j)
+                sum=(num1.charAt(i)-'0')*(num2.charAt(j)-'0')+increment;
+                buffer.add(sum%10);
+                increment=sum/10;
+            }
+            if (increment>0)
+            {
+                buffer.add(increment);
+                increment=0;
+            }
+            // combine with the previous result
+            if (i==num1.length()-1)
+            {
+                result.addAll(buffer);
+            }
+            else
+            {
+                for (int l=0; l<buffer.size(); l++)
                 {
-                    z.add((num1.charAt(i)-'0')*(num2.charAt(j)-'0'));
+                    if (result.size()<l+num1.length()-i)
+                    {
+                        sum=buffer.get(l)+increment;
+                        result.add(sum%10);
+                        increment=sum/10;
+                    }
+                    else
+                    {
+                        sum=result.get(l+num1.length()-i-1)+buffer.get(l)+increment;
+                        result.set(l+num1.length()-i-1, sum%10);
+                        increment=sum/10;
+                    }
                 }
-                else
+                if (increment>0)
                 {
-                    z.set(i+j, z.get(i+j)+(num1.charAt(i)-'0')*(num2.charAt(j)-'0'));
+                    if (result.size()<buffer.size()+num1.length()-i)
+                    {
+                        result.add(increment);
+                    }
+                    else
+                    {
+                        result.set(buffer.size()+num1.length()-i-1, increment);
+                    }
+                    increment=0;
                 }
             }
         }
-        // post-processing: make every entry to be a single digits
-        int increment=0;
-        StringBuffer result=new StringBuffer();
-        for (int i=z.size()-1; i>=0; i--)
+        // convert to the string
+        StringBuffer str=new StringBuffer();
+        boolean head0=true;
+        for(int i=result.size()-1; i>=0; i--)
         {
-            // we keep the last digit and shift the remaining to the digits up
-            increment+=z.get(i);
-            result.insert(0, increment%10);
-            increment=increment/10;
-        }
-        // for the last digits
-        if (increment>0)
-        {
-            result.insert(0, increment);
-        }
-        // remove the 0 at the begining
-        int i=0;
-        for (i=0; i<result.length(); i++)
-        {
-            if (result.charAt(i)!='0')
+            if (!head0 || result.get(i)!=0)
             {
-                break;
+                str.append(result.get(i));
+                head0=false;
             }
         }
-        if (i>=result.length())
+        if (str.length()<1)
         {
-            // just return '0'
-            return result.substring(0, 1);
+        	return "0";
         }
         else
         {
-            return result.substring(i, result.length());
-        }
+        	return str.toString();
+    	}
     }
-    
-    private ArrayList<Integer> string2Array(String num)
-    {
-        ArrayList<Integer> arr=new ArrayList<Integer>();
-        for (int i=0; i<num.length(); i++)
-        {
-            arr.add(num.charAt(i)-'0');
-        }
-        return arr;
-    }
-}
+}	
