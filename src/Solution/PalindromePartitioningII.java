a727c7358ae6c62d330f87c4d6c2603116be8b6e
zhangtemplar <zhangtemplar@gmail.com>
2013-11-05 00:30:57 -0700

    Update PalindromePartitioningII.java

--- src/Solution/PalindromePartitioningII.java
+++ src/Solution/PalindromePartitioningII.java
@@ -1,190 +1,87 @@
-public class Solution {
-    public String minWindow(String S, String T) {
+package trial2;
+
+import java.util.HashMap;
+
+public class PalindromePartitioningII {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PalindromePartitioningII instance=new PalindromePartitioningII();
+		System.out.println(instance.minCut("apjesgpsxoeiokmqmfgvjslcjukbqxpsobyhjpbgdfruqdkeiszrlmtwgfxyfostpqczidfljwfbbrflkgdvtytbgqalguewnhvvmcgxboycffopmtmhtfizxkmeftcucxpobxmelmjtuzigsxnncxpaibgpuijwhankxbplpyejxmrrjgeoevqozwdtgospohznkoyzocjlracchjqnggbfeebmuvbicbvmpuleywrpzwsihivnrwtxcukwplgtobhgxukwrdlszfaiqxwjvrgxnsveedxseeyeykarqnjrtlaliyudpacctzizcftjlunlgnfwcqqxcqikocqffsjyurzwysfjmswvhbrmshjuzsgpwyubtfbnwajuvrfhlccvfwhxfqthkcwhatktymgxostjlztwdxritygbrbibdgkezvzajizxasjnrcjwzdfvdnwwqeyumkamhzoqhnqjfzwzbixclcxqrtniznemxeahfozp"));
+	}
+
+	public int minCut(String s) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        // we will first set the window to the whole S
-        // if the windows contains necessary char of T
-        // we shrink on the left until the condition holds
-        // otherwise, we take a step back and shrink on the right
-        if (S==null || T==null || S.length()<T.length())
+        // no obvious efficient solution, we try greedy search first
+        if(s==null || s.length()<2)
         {
-            return "";
+            return 0;
         }
-        int left=0;
-        int right=T.length()-1;
-        QuickTable table=new QuickTable();
-        for (int i=0; i<T.length(); i++)
+        return minCut(s, 0, s.length()-1, new HashMap<Integer, Integer>(), new HashMap<Integer, Boolean>());
+    }
+    
+	/**
+     * @param s the input string
+     * @param start the end of previous exploration
+     * @param max the maximal number of cut still allowed to get a better solution
+     * @param buffer_cut the buffer for the # cut required for s[start:end]
+     * @param buffer_palindrome the buffer for defining palindrome
+     * @output the min # of cuts required for s[start:end]
+     */ 
+    private int minCut(String s, int start, int max, HashMap<Integer, Integer> buffer_cut, HashMap<Integer, Boolean> buffer_palindrome)
+    {
+    	if (buffer_cut.containsKey(start))
         {
-            table.remove(T.charAt(i));
-            table.add(S.charAt(i));
+            return buffer_cut.get(start);
         }
-        int min=Integer.MAX_VALUE;
-        int []result=new int[2];
-        result[0]=0;
-        result[1]=S.length()-1;
-        while(right<S.length())
+        else if (start>=s.length())
         {
-            if (table.isValid())
+            return -1;
+        }
+    	if (max<=0)
+    	{
+    		return 0;
+    	}
+        else
+        {
+            // we the starting point as the number of remaining cut we have
+            int result=Math.min(max-1, s.length()-start-1);
+            for (int i=s.length()-1; i>=start; i--)
             {
-                if (min>right-left+1)
+                // a parlindrome find
+                if (isPalindrome(s, start, i, buffer_palindrome))
                 {
-                    min=right-left+1;
-                    result[0]=left;
-                    result[1]=right;
+                    result=Math.min(result, minCut(s, i+1, result, buffer_cut, buffer_palindrome));
                 }
-                table.remove(S.charAt(left++));
             }
-            else if(right<S.length()-1)
-            {
-                table.add(S.charAt(++right));
-            }
-            else
-            {
-                break;
-            }
+            result++;
+            buffer_cut.put(start, result);
+            return result;
         }
-        if (min>S.length())
+    }
+    
+    private boolean isPalindrome(String s, int start, int end, HashMap<Integer, Boolean> buffer)
+    {
+        if (buffer.containsKey(start*s.length()+end))
         {
-            return "";
+            return buffer.get(start*s.length()+end);
         }
         else
         {
-            return S.substring(result[0], result[1]+1);
-        }
-    }
-}
-
-class QuickTable
-{
-    private TableNode head, tail;
-    private HashMap<Character, TableNode> table;
-    
-    public QuickTable()
-    {
-        head=null;
-        tail=null;
-        table=new HashMap<Character, TableNode>();
-    }
-    
-    // remove the couning of a word
-    public void remove(char c)
-    {
-        // check whether we have a count for it
-        if (table.containsKey(c))
-        {
-            // found a node
-            TableNode node=table.get(c);
-            // decrease the count
-            node.val--;
-            if (node.val>=0 || node==head)
+            for (int i=start; i<end-i+start; i++)
             {
+                if (s.charAt(i)!=s.charAt(end-i+start))
+                {
+                    buffer.put(start*s.length()+end, false);
+                    return false;
+                }
             }
-            // if it converts to -1, we move it to the head
-            else if(node==tail)
-            {
-                tail=node.prev;
-                node.prev.next=null;
-                node.prev=null;
-                head.prev=node;
-                node.next=head;
-                head=node;
-            }
-            else
-            {
-                node.prev.next=node.next;
-                node.next.prev=node.prev;
-                node.next=head;
-                head.prev=node;
-                head=node;
-            }
+            buffer.put(start*s.length()+end, true);
+            return true;
         }
-        else
-        {
-            TableNode node=new TableNode(c, -1);
-            table.put(c, node);
-            node.next=head;
-            if (head!=null)
-            {
-            	head.prev=node;
-            }
-            head=node;
-        }
-        // for singleton case
-        if (tail==null)
-        {
-            tail=head;
-        }
-    }
-    
-    public void add(char c)
-    {
-        // check whether we have a count for it
-        if (table.containsKey(c))
-        {
-            // found a node
-            TableNode node=table.get(c);
-            node.val++;
-            // decrease the count
-            if (node.val<0 || node==tail)
-            {
-            }
-            // if it converts to 0, we move it to the tail
-            else if(node==head)
-            {
-                head=head.next;
-                head.prev=null;
-                node.next=null;
-                node.prev=tail;
-                tail.next=node;
-                tail=node;
-            }
-            else
-            {
-                node.prev.next=node.next;
-                node.next.prev=node.prev;
-                node.prev=tail;
-                tail.next=node;
-                tail=node;
-            }
-        }
-        else
-        {
-            TableNode node=new TableNode(c, 1);
-            table.put(c, node);
-            node.prev=tail;
-            if (tail!=null)
-            {
-            	tail.next=node;
-        	}
-            tail=node;
-        }
-        // for singleton case
-        if (head==null)
-        {
-            head=tail;
-        }
-    }
-    
-    public boolean isValid()
-    {
-        if (head!=null)
-        {
-            return head.val>=0;
-        }
-        return true;
-    }
-}
-
-class TableNode
-{
-    public char c;
-    public int val;
-    public TableNode next, prev;
-    public TableNode(char ch, int x)
-    {
-        c=ch;
-        val=x;
-        next=null;
-        prev=null;
     }
 }
