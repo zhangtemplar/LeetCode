66feddd4948e8d275c0c57559cce55117787a375
zhangtemplar <zhangtemplar@gmail.com>
2013-11-05 00:01:20 -0700

    Update DecodeWays.java
    Submission Result: Compile Error
    
    Line 10: ';' expected
    
    Submission Result: Wrong Answer
    
    Input:	"1"
    Output:	0
    Expected:	1

--- src/Solution/DecodeWays.java
+++ src/Solution/DecodeWays.java
@@ -1,91 +1,44 @@
-package Solution;
-
-import java.util.HashMap;
-
-public class DecodeWays {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		DecodeWays instance=new DecodeWays();
-		System.out.println(instance.numDecodings("1787897759966261825913315262377298132516969578441236833255596967132573482281598412163216914566534565"));
-	}
-
-	public int numDecodings(String s) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we may use recursion
+public class Solution {
+    public int numDecodings(String s) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        // we use dynamic programming
         if (s==null || s.length()<1)
         {
             return 0;
         }
-        set=new HashMap<Integer, Integer>();
-        return numDecodings(s, 0);
+        return numDecodings(s, 0, new HashMap<Integer, Integer>());
     }
-
-	private HashMap<Integer, Integer> set;
-	
-    private int numDecodings(String s, int start)
+    
+    private int numDecodings(String s, int start, HashMap<Integer, Integer> buffer)
     {
-    	int result;
-    	if (start>s.length())
+        if(buffer.containsKey(start))
         {
-            return 0;
+            return buffer.get(start);
         }
-    	else if (start==s.length())
-    	{
-    		return 1;
-    	}
-        else if (s.charAt(start)=='0')
+        else if(start>=s.length())
         {
-            return 0;
-        }
-        else if (start==s.length()-1)
-        {
-        	return 1;
-        }
-        else if (set.containsKey(start))
-        {
-        	return set.get(start);
-        }
-        else if (s.charAt(start)>'2')
-        {
-            return numDecodings(s, start+1);
-        }
-        else if (s.charAt(start)=='2')
-        {
-            // we need to decide
-            if (s.charAt(start+1)>'6')
-            {
-            	result=numDecodings(s, start+1);
-            }
-            else if (s.charAt(start+1)=='0')
-            {
-            	// 20
-            	result=numDecodings(s, start+2);
-            }
-            else
-            {
-            	result=numDecodings(s, start+1)+numDecodings(s, start+2);
-            }
+            return 1;
         }
         else
         {
-        	// the end of string
-            if (s.charAt(start+1)=='0')
+            int result=0;
+            if (s.charAt(start)=='0')
             {
-            	// 10
-            	result=numDecodings(s, start+2);
+                // illegal case
+                result=0;
+            }
+            else if (start<s.length()-1 && ((s.charAt(start)-'0')*10+(s.charAt(start+1)-'0'))<=26)
+            {
+                // we can either have a double or single
+                result=numDecodings(s, start+1, buffer)+numDecodings(s, start+2, buffer);
             }
             else
             {
-            	// 11, 12, ...
-            	result=numDecodings(s, start+1)+numDecodings(s, start+2);
+                result=numDecodings(s, start+1, buffer);
             }
+            buffer.put(start, result);
+            return result;
         }
-    	set.put(start, result);
-    	return result;
     }
 }
