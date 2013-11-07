2dcef3232120460e32cb92e88128428442cacbe1
zhangtemplar <zhangtemplar@gmail.com>
2013-11-06 22:45:37 -0700

    Update DivideInteger.java
    Submission Result: Compile Error
    
    Line 6: ')' expected

--- src/Solution/DivideInteger.java
+++ src/Solution/DivideInteger.java
@@ -1,115 +1,44 @@
-package Solution;
-
-public class DivideInteger {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		DivideInteger instance=new DivideInteger();
-		System.out.println(instance.divide(2147483647, 1));
-	}
-
-	public int divideSlow(int dividend, int divisor)
-	{
-		int sign=1;
-		long dd=dividend;
-		long ds=divisor;
-		// for the sign
-		if (dd<0)
-		{
-			sign=-sign;
-			dd=-dd;
-		}
-		if (ds<0)
-		{
-			sign=-sign;
-			ds=-ds;
-		}
-		// divide zero
-		if (ds==0)
-		{
-			if (sign>0)
-			{
-				return Integer.MAX_VALUE;
-			}
-			else
-			{
-				return Integer.MIN_VALUE;
-			}
-		}
-		long multiplier, result;
-		multiplier=ds;
-		result=0;
-		while (multiplier<dd)
-		{
-			multiplier+=ds;
-			result++;
-		}
-		return (int) (sign*result);
-	}
-	
-	public int divide(int dividend, int divisor) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-		// we need to make sure there is no overflow
-		long dd=dividend;
-		long ds=divisor;
-		// for the sign
-        int sign=1;
-		if (dd<0)
-		{
-			sign=-sign;
-			dd=-dd;
-		}
-		if (ds<0)
-		{
-			sign=-sign;
-			ds=-ds;
-		}
-		// divide zero
-		if (ds==0)
-		{
-			if (sign>0)
-			{
-				return Integer.MAX_VALUE;
-			}
-			else
-			{
-				return Integer.MIN_VALUE;
-			}
-		}
-        long sum, result, multiplier, step;
-        sum=0;
-        result=0;
-        while(true)
+public class Solution {
+    public int divide(int dividend, int divisor) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        // we will use long to avoid overflow
+        return (int) divide((long) dividend, (long) divisor);
+    }
+    
+    private long divide(long dividend, long divisor)
+    {
+        // some of the special case
+        if (dividend<0)
         {
-            multiplier=ds;
-            step=1;
-            // we double the guess (multiplier, step)
-            // if dd=Integer.MAX_VALIE and sum or multiplier is int, this step will never terminate
-            // so we need to make it as long
-            while (sum+multiplier<=dd)
-            {
-                multiplier+=multiplier;
-                step+=step;
-            }
-            multiplier=multiplier>>>1;
-            step=step>>>1;
-            // check the condition
-            // this means we already found the solution
-            if (step<=1)
-            {
-                break;
-            }
-            else
-            {
-                // otherwise, we put this in and found the next one
-                sum+=multiplier;
-                result+=step;
-            }
+            return -divide(-dividend, divisor);
         }
-        return (int) (sign*(result+step));
+        if (divisor<0)
+        {
+            return -divide(dividend, -divisor);
+        }
+        if (dividend==0 || dividend==0)
+        {
+            return 0;
+        }
+        if (dividend<divisor)
+        {
+            return 0;
+        }
+        // we will use binary search
+        long result=0;
+        while(dividend>=divisor)
+        {
+            long tmp=divisor;
+            long current=1;
+            while(dividend>(tmp<<1))
+            {
+                tmp=tmp<<1;
+                current=current<<1;
+            }
+            result+=current;
+            dividend-=tmp;
+        }
+        return result;
     }
 }
