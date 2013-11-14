e35ed887283c7f9c3fdb7c028035e757474021fe
zhangtemplar <zhangtemplar@gmail.com>
2013-11-13 17:57:33 -0700

    Update PopulatingNextRightPointersEachNode.java

--- src/Solution/PopulatingNextRightPointersEachNode.java
+++ src/Solution/PopulatingNextRightPointersEachNode.java
@@ -1,67 +1,25 @@
-package Solution;
-
-import java.util.HashMap;
-import java.util.LinkedList;
-
-public class PopulatingNextRightPointersEachNode {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void connect(TreeLinkNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        // we can a level order traversal
-        // use level-order traverse
-        LinkedList<TreeLinkNode> queue=new LinkedList<TreeLinkNode>();
-        HashMap<TreeLinkNode, Integer> level=new HashMap<TreeLinkNode, Integer>();
-        if (root==null)
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // we will use level order traverse
+        if (root==null || root.left==null || root.right==null)
         {
             return;
         }
-        queue.add(root);
-        level.put(root, 0);
-        TreeLinkNode node, prev;
-        node=null;
-        prev=null;
-        int height=0;
-        while(!queue.isEmpty())
+        root.left.next=root.right;
+        if(root.next!=null)
         {
-            node=queue.remove();
-            // add its children and their height accordingly
-            if (node.left!=null)
-            {
-                queue.add(node.left);
-                level.put(node.left, level.get(node)+1);
-            }
-            if (node.right!=null)
-            {
-                queue.add(node.right);
-                level.put(node.right, level.get(node)+1);
-            }
-            // check whether we have finished the level or not
-            if (level.get(node)!=height)
-            {
-                // we finish the previous level
-                prev=null;
-                height=level.get(node);
-            }
-            if (prev!=null)
-            {
-                prev.next=node;
-            }
-            prev=node;
+            root.right.next=root.next.left;
         }
+        connect(root.left);
+        connect(root.right);
     }
-}
-
-class TreeLinkNode {
-    int val;
-    TreeLinkNode left, right, next;
-    TreeLinkNode(int x) { val = x; }
 }
